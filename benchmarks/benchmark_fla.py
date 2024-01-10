update Based benchmark. Fix Based gradient bug.

benchmarks/benchmark_fla.py
@@ -2,7 +2,11 @@
 
 import torch
 import triton
-from flash_attn import flash_attn_func
+try:
+    from flash_attn import flash_attn_func
+    HAS_FLASH = True
+except ImportError:
+    HAS_FLASH = False
 
 from fla.ops.triton.based import parallel_based
 from fla.ops.triton.retention import fused_chunk_retention
@@ -17,11 +21,12 @@ from fla.ops.triton.retention import fused_chunk_retention
         # argument name whose value corresponds to a different line in the plot
         line_arg='provider',
         # possible values for `line_arg``
-        line_vals=['flash', 'retention', 'based'],
+        line_vals=['retention', 'based'] + (['flash'] if HAS_FLASH else []),
         # label name for the lines
-        line_names=['flash', 'retention', 'based'],
+        line_names=['retention', 'based'] + (['flash'] if HAS_FLASH else []),
         # line styles
-        styles=[('green', '-'), ('blue', '--'), ('red', '-.'), ('cyan', ':'), ('yellow', 'dotted'), ('black', 'dashed')],
+        styles=[('green', '-'), ('blue', '--'), ('red', '-.'), ('cyan', ':')] + \
+        ([('yellow', 'dotted'), ('black', 'dashed')] if HAS_FLASH else []),
         ylabel="Execution Time (ms)",  # label name for the y-axis
         # name for the plot. Used also as a file name for saving the plot.
         plot_name="Performance",
@@ -35,23 +40,32 @@ def benchmark(seq_len, provider):
     batch_size, n_heads, d_head = 8, 32, 128
 
     if provider == 'flash':
-        q = torch.randn(batch_size, seq_len, n_heads, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
-        k = torch.randn(batch_size, seq_len, n_heads, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
-        v = torch.randn(batch_size, seq_len, n_heads, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
+        q = torch.randn(batch_size, seq_len, n_heads, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
+        k = torch.randn(batch_size, seq_len, n_heads, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
+        v = torch.randn(batch_size, seq_len, n_heads, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
     else:
-        q = torch.randn(batch_size, n_heads, seq_len, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
-        k = torch.randn(batch_size, n_heads, seq_len, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
-        v = torch.randn(batch_size, n_heads, seq_len, d_head, device=device, requires_grad=requires_grad, dtype=dtype)
+        q = torch.randn(batch_size, n_heads, seq_len, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
+        k = torch.randn(batch_size, n_heads, seq_len, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
+        v = torch.randn(batch_size, n_heads, seq_len, d_head,
+                        device=device, requires_grad=requires_grad, dtype=dtype)
     do = torch.ones_like(q, dtype=dtype)
 
     quantiles = [0.5, 0.2, 0.8]
     results = 0, 0, 0
     if provider == 'flash':
-        results = triton.testing.do_bench(lambda: flash_attn_func(q, k, v).backward(do), quantiles=quantiles)
+        results = triton.testing.do_bench(lambda: flash_attn_func(
+            q, k, v).backward(do), quantiles=quantiles)
     elif provider == 'retention':
-        results = triton.testing.do_bench(lambda: fused_chunk_retention(q, k, v).backward(do), quantiles=quantiles)
+        results = triton.testing.do_bench(lambda: fused_chunk_retention(
+            q, k, v).backward(do), quantiles=quantiles)
     elif provider == 'based':
-        results = triton.testing.do_bench(lambda: parallel_based(q, k, v).backward(do), quantiles=quantiles)
+        results = triton.testing.do_bench(lambda: parallel_based(
+            q, k, v).backward(do), quantiles=quantiles)
     return results
 
 

fla/layers/gla.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Retentive Network: A Successor to Transformer for Large Language Models"[https://arxiv.org/pdf/2307.08621.pdf]
+# "Gated Linear Attention Transformers with Hardware-Efficient Training"[https://arxiv.org/abs/2312.06635]
 
 from fla.ops.triton.gla import fused_chunk_gla, chunk_gla, fused_recurrent_gla
 from fla.module.rmsnorm import RMSNorm
 from einops import rearrange
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
 
 def get_activation_fn(activation):
     if activation == "swish":
@@ -20,7 +20,7 @@ def get_activation_fn(activation):
 
 class GatedLinearAttention(nn.Module):
     def __init__(self,
-                 embed_dim=1024,
+                 d_model=1024,
                  expansion_ratio=0.5,
                  num_heads=4,
                  gate_fn="swish",
@@ -29,20 +29,20 @@ class GatedLinearAttention(nn.Module):
                  gate_low_rank_dim=16,  
                  *args, **kwargs):
         super().__init__()
-        self.embed_dim = embed_dim
-        self.value_dim = embed_dim 
-        self.key_dim = int(embed_dim * expansion_ratio)
+        self.d_model = d_model
+        self.value_dim = d_model 
+        self.key_dim = int(d_model * expansion_ratio)
         self.num_heads = num_heads
         self.head_qk_dim = self.key_dim // num_heads
         self.head_v_dim = self.value_dim // num_heads
         self.gate_fn = get_activation_fn(activation=str(gate_fn))
-        self.q_proj = nn.Linear(embed_dim, self.key_dim, bias=False)
-        self.k_proj = nn.Linear(embed_dim, self.key_dim, bias=False)
-        self.v_proj = nn.Linear(embed_dim, self.value_dim, bias=False)
-        self.g_proj = nn.Linear(embed_dim, self.value_dim, bias=False)
-        self.gk_proj = nn.Sequential(nn.Linear(embed_dim,  gate_low_rank_dim, bias=False),
+        self.q_proj = nn.Linear(d_model, self.key_dim, bias=False)
+        self.k_proj = nn.Linear(d_model, self.key_dim, bias=False)
+        self.v_proj = nn.Linear(d_model, self.value_dim, bias=False)
+        self.g_proj = nn.Linear(d_model, self.value_dim, bias=False)
+        self.gk_proj = nn.Sequential(nn.Linear(d_model,  gate_low_rank_dim, bias=False),
                                      nn.Linear(gate_low_rank_dim, self.key_dim, bias=True))
-        self.out_proj = nn.Linear(self.value_dim, embed_dim, bias=False)
+        self.out_proj = nn.Linear(self.value_dim, d_model, bias=False)
         self.group_norm = RMSNorm(self.head_v_dim, eps=layernorm_eps)
         self.gate_logit_normalizer = gate_logit_normalizer
         self.reset_parameters()
@@ -56,8 +56,8 @@ class GatedLinearAttention(nn.Module):
         nn.init.xavier_uniform_(self.gk_proj[0].weight, gain=2 ** -2.5)
         nn.init.xavier_uniform_(self.gk_proj[1].weight, gain=2 ** -2.5)
 
-    def forward(self, x, form='chunk'):
-        assert form in ['fused_chunk', 'chunk', 'fused_recurrent']
+    def forward(self, x, fwd_mode='chunk'):
+        assert fwd_mode in ['fused_chunk', 'chunk', 'fused_recurrent']
         assert x.shape[-1] % 16 == 0, "only support dimension divisible by 16 for now" 
         assert x.shape[-2] % 16 == 0, "only support input length divisible by 16 for now"
         q = rearrange(self.q_proj(
@@ -70,12 +70,13 @@ class GatedLinearAttention(nn.Module):
         g = F.logsigmoid(g) / self.gate_logit_normalizer
         g = rearrange(g, 'b n (h d) -> b h n d',
                       h=self.num_heads)
-        if form == 'fused_chunk':
+        if fwd_mode == 'fused_chunk':
             o = fused_chunk_gla(q, k, v, g)
-        elif form == 'chunk':
+        elif fwd_mode == 'chunk':
+            # for numumerical stable consideration
             g = torch.clamp(g, min=-3)
             o = chunk_gla(q, k, v, g)
-        elif form == 'fused_recurrent':
+        elif fwd_mode == 'fused_recurrent':
             o = fused_recurrent_gla(q, k, v, g)
         else:
             raise NotImplementedError

fla/layers/multiscale_retention.py
@@ -21,24 +21,24 @@ def get_activation_fn(activation):
 
 class MultiScaleRetention(nn.Module):
     def __init__(self,
-                 embed_dim=1024,
+                 d_model=1024,
                  expansion_ratio=2,
                  num_heads=4,
                  gate_fn="swish",
                  layernorm_eps=1e-5,
                  *args, **kwargs):
         super().__init__()
-        self.embed_dim = embed_dim
-        self.value_dim = embed_dim * expansion_ratio
+        self.d_model = d_model
+        self.value_dim = d_model * expansion_ratio
         self.num_heads = num_heads
-        self.head_qk_dim = self.embed_dim // num_heads
+        self.head_qk_dim = self.d_model // num_heads
         self.head_v_dim = int(self.head_qk_dim * expansion_ratio)
         self.gate_fn = get_activation_fn(activation=str(gate_fn))
-        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.v_proj = nn.Linear(embed_dim, self.value_dim, bias=False)
-        self.g_proj = nn.Linear(embed_dim, self.value_dim, bias=False)
-        self.out_proj = nn.Linear(self.value_dim, embed_dim, bias=False)
+        self.q_proj = nn.Linear(d_model, d_model, bias=False)
+        self.k_proj = nn.Linear(d_model, d_model, bias=False)
+        self.v_proj = nn.Linear(d_model, self.value_dim, bias=False)
+        self.g_proj = nn.Linear(d_model, self.value_dim, bias=False)
+        self.out_proj = nn.Linear(self.value_dim, d_model, bias=False)
 
         self.group_norm = RMSNorm(self.head_v_dim, eps=layernorm_eps)
         self.rotary = RotaryEmbedding(dim=self.head_qk_dim, interleaved=False)
@@ -51,8 +51,8 @@ class MultiScaleRetention(nn.Module):
         nn.init.xavier_uniform_(self.g_proj.weight, gain=2 ** -2.5)
         nn.init.xavier_uniform_(self.out_proj.weight, gain=2 ** -1)
 
-    def forward(self, x, form='fused_chunk'):
-        assert form in ['fused_chunk', 'parallel', 'chunk', 'fused_recurrent']
+    def forward(self, x, fwd_mode='fused_chunk'):
+        assert fwd_mode in ['fused_chunk', 'parallel', 'chunk', 'fused_recurrent']
         q1 = rearrange(self.q_proj(
             x), '... (h d) -> ... h d', h=self.num_heads)
         k1 = rearrange(self.k_proj(
@@ -61,11 +61,11 @@ class MultiScaleRetention(nn.Module):
         q, k = q.transpose(1, 2), k.transpose(1, 2)
         v = rearrange(self.v_proj(x), 'b n (h d) -> b h n d',
                       h=self.num_heads).contiguous()
-        if form == 'fused_chunk':
+        if fwd_mode == 'fused_chunk':
             o = fused_chunk_retention(q, k, v)
-        elif form == 'parallel':
+        elif fwd_mode == 'parallel':
             o = parallel_retention(q, k, v)
-        elif form == 'fused_recurrent':
+        elif fwd_mode == 'fused_recurrent':
             o = fused_recurrent_retention(q, k, v)
         # TODO: need fix to allow different d_head_qk and d_head_v for "chunk" form
         else:

fla/ops/torch/based.py
@@ -77,14 +77,14 @@ if __name__ == "__main__":
     B = 4
     H = 4
     L = 128
-    D = 15
+    # D = 15
     dtype = torch.float32
-    q = (torch.randn(B, H, L, D).cuda().to(dtype) / 10).requires_grad_(True)
-    k = (torch.randn(B, H, L, D).cuda().to(dtype) / 10).requires_grad_(True)
-    v = torch.randn(B, H, L, D).cuda().to(dtype).requires_grad_(True)
-
-    do = torch.randn_like(v).cuda() / 10
-    ref = torch_parallel_based(q, k, v, True, False)
+    q = (torch.randn(B, H, L, 16).cuda().to(dtype)).requires_grad_(True)
+    k = (torch.randn(B, H, L, 16).cuda().to(dtype)).requires_grad_(True)
+    v = torch.randn(B, H, L, 128).cuda().to(dtype).requires_grad_(True)
+    
+    do = torch.randn_like(v).cuda() 
+    ref = torch_parallel_based(q, k, v, True, True)
     ref.backward(do, retain_graph=True)
     ref_dq, q.grad = q.grad.clone(), None
     ref_dk, k.grad = k.grad.clone(), None
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     # assert ref_dk.allclose(tri_dk, 0, 1e-4), breakpoint()
     # assert ref_dv.allclose(tri_dv, 0, 1e-4), breakpoint()
 
-    tri = fused_chunk_based_dim16(q, k, v, True, False)
+    tri = fused_chunk_based_dim16(q, k, v, True, True)
     tri.backward(do, retain_graph=True)
     tri_dq, q.grad = q.grad.clone(), None
     tri_dk, k.grad = k.grad.clone(), None
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     # assert ref_dk.allclose(tri_dk, 0, 1e-4), breakpoint()
     # assert ref_dv.allclose(tri_dv, 0, 1e-4), breakpoint()
 
-    tri = parallel_based(q, k, v, True, False)
+    tri = parallel_based(q, k, v, True, True)
     tri.backward(do, retain_graph=True)
     tri_dq, q.grad = q.grad.clone(), None
     tri_dk, k.grad = k.grad.clone(), None