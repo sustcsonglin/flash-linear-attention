6be55afe5009f94bd61e05b57a77a5d4062dca4b
yzhangcs <yzhang.cs@outlook.com>
2024-01-22 02:11:30 +0800

    Show diffs

--- tests/ops/test_gla.py
+++ tests/ops/test_gla.py
@@ -2,22 +2,23 @@
 
 import pytest
 import torch
+import torch.nn.functional as F
 
 from fla.ops.triton.gla import chunk_gla, fused_chunk_gla
-from torch.nn.functional import logsigmoid
 
-@pytest.mark.parametrize("dtype", [torch.float32])
+
+@pytest.mark.parametrize("dtype", [torch.float])
 @pytest.mark.parametrize("D", [32, 64, 128])
 @pytest.mark.parametrize("T", [512])
 def test_gla(dtype, D, T, B=4, H=4):
+    atol = 1e-2 if dtype == torch.float else 1e-1
     # [batch_size, n_heads, seq_len, d_head]
-    q = (torch.randn((B, H, T, D), dtype=dtype, device='cuda') / 10).requires_grad_()
-    k = (torch.randn((B, H, T, D), dtype=dtype, device='cuda') / 10).requires_grad_()
-    v = (torch.randn((B, H, T, D), dtype=dtype, device='cuda')).requires_grad_()
+    q = torch.randn((B, H, T, D), dtype=dtype, device='cuda').requires_grad_()
+    k = torch.randn((B, H, T, D), dtype=dtype, device='cuda').requires_grad_()
+    v = torch.randn((B, H, T, D), dtype=dtype, device='cuda').requires_grad_()
     g = torch.randn((B, H, T, D), dtype=dtype, device='cuda')
-    g = logsigmoid(g).clamp_min(-3)
-    g.requires_grad_(True)
-    do = torch.randn_like(v) 
+    g = F.logsigmoid(g).clamp_min(-3).requires_grad_(True)
+    do = torch.randn_like(v)
     ref = chunk_gla(q, k, v, g)
     ref.backward(do)
     ref_dq, q.grad = q.grad.clone(), None
@@ -33,11 +34,8 @@ def test_gla(dtype, D, T, B=4, H=4):
     tri_dv, v.grad = v.grad.clone(), None
     tri_dg, g.grad = g.grad.clone(), None
 
-    assert ref.allclose(tri, 0, 1e-4)
-    assert ref_dq.allclose(tri_dq, 0, 1e-4)
-    assert ref_dk.allclose(tri_dk, 0, 1e-4)
-    assert ref_dv.allclose(tri_dv, 0, 1e-4)
-    assert ref_dg.allclose(tri_dg, 0, 1e-4)
-
-    print('Done!')
-
+    assert ref.allclose(tri, 0, atol), f"o diff: {torch.abs(ref - tri).max()}"
+    assert ref_dq.allclose(tri_dq, 0, atol), f"dq diff: {torch.abs(ref_dq - tri_dq).max()}"
+    assert ref_dk.allclose(tri_dk, 0, atol), f"dk diff: {torch.abs(ref_dk - tri_dk).max()}"
+    assert ref_dv.allclose(tri_dv, 0, atol), f"dv diff: {torch.abs(ref_dv - tri_dv).max()}"
+    assert ref_dg.allclose(tri_dg, 0, atol), f"dg diff: {torch.abs(ref_dg - tri_dg).max()}"
