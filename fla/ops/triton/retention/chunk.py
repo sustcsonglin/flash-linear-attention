a1c959c6d04ad50a8b773b1604b20f3acf5b6ce0
Yu Zhang <yzhang.cs@outlook.com>
2024-01-23 23:52:07 +0800

    Parallelism of dqkv along the k dim with dv reduction

--- fla/ops/triton/retention/chunk.py
+++ fla/ops/triton/retention/chunk.py
@@ -208,7 +208,8 @@ def chunk_retention_bwd_kernel_dqkv(
     BK: tl.constexpr,
     BV: tl.constexpr
 ):
-    i_t, i_bh = tl.program_id(0), tl.program_id(1)
+    i_k, i_t, i_bh = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+    n_bh = tl.num_programs(2)
     i_h = i_bh % H
     b_b = tl.math.log2(1 - tl.math.pow(2, -5 - i_h * 1.0))
 
@@ -218,57 +219,55 @@ def chunk_retention_bwd_kernel_dqkv(
     m_s = o_i[:, None] >= o_i[None, :]
     d_s = tl.where(m_s, tl.math.exp2((o_i[:, None] - o_i[None, :]) * b_b), 0) * scale
 
-    for i_k in range(0, tl.cdiv(DK, BK)):
-        p_q = tl.make_block_ptr(q + i_bh * s_qk_h, (DK, T), (s_qk_d, s_qk_t), (i_k * BK, i_t * BT), (BK, BT), (0, 1))
-        p_k = tl.make_block_ptr(k + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
+    p_q = tl.make_block_ptr(q + i_bh * s_qk_h, (DK, T), (s_qk_d, s_qk_t), (i_k * BK, i_t * BT), (BK, BT), (0, 1))
+    p_k = tl.make_block_ptr(k + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
 
-        p_v = tl.make_block_ptr(v + i_bh * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
-        p_h = tl.make_block_ptr(h + i_bh * s_hh, (DV, TDK), (1, s_ht), (0, i_t * DK + i_k * BK), (BV, BK), (0, 1))
-        p_do = tl.make_block_ptr(do + i_bh * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
-        p_dh = tl.make_block_ptr(dh + i_bh * s_hh, (TDK, DV), (s_ht, 1), (i_t * DK + i_k * BK, 0), (BK, BV), (1, 0))
-        p_dv = tl.make_block_ptr(dv + i_bh * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
+    p_v = tl.make_block_ptr(v + i_bh * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
+    p_h = tl.make_block_ptr(h + i_bh * s_hh, (DV, TDK), (1, s_ht), (0, i_t * DK + i_k * BK), (BV, BK), (0, 1))
+    p_do = tl.make_block_ptr(do + i_bh * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
+    p_dh = tl.make_block_ptr(dh + i_bh * s_hh, (TDK, DV), (s_ht, 1), (i_t * DK + i_k * BK, 0), (BK, BV), (1, 0))
+    p_dv = tl.make_block_ptr(dv + (i_k * n_bh + i_bh) * s_vo_h, (T, DV), (s_vo_t, s_vo_d), (i_t * BT, 0), (BT, BV), (1, 0))
 
-        p_dq = tl.make_block_ptr(dq + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
-        p_dk = tl.make_block_ptr(dk + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
+    p_dq = tl.make_block_ptr(dq + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
+    p_dk = tl.make_block_ptr(dk + i_bh * s_qk_h, (T, DK), (s_qk_t, s_qk_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
 
-        b_q = tl.load(p_q, boundary_check=(0, 1))
-        b_k = tl.load(p_k, boundary_check=(0, 1))
-        b_s = tl.dot(b_k, b_q, allow_tf32=False) * tl.trans(d_s)
+    b_q = tl.load(p_q, boundary_check=(0, 1))
+    b_k = tl.load(p_k, boundary_check=(0, 1))
+    b_s = tl.dot(b_k, b_q, allow_tf32=False) * tl.trans(d_s)
 
-        b_dq = tl.zeros([BT, BK], dtype=tl.float32)
-        b_dk = tl.zeros([BT, BK], dtype=tl.float32)
-        for _ in range(tl.cdiv(DV, BV)):
-            # [BT, BV]
-            b_v = tl.load(p_v, boundary_check=(0, 1))
-            b_do = tl.load(p_do, boundary_check=(0, 1))
-            # [BV, BK]
-            b_h = tl.load(p_h, boundary_check=(0, 1))
-            # [BK, BV]
-            b_dh = tl.load(p_dh, boundary_check=(0, 1))
+    b_dq = tl.zeros([BT, BK], dtype=tl.float32)
+    b_dk = tl.zeros([BT, BK], dtype=tl.float32)
+    for _ in range(tl.cdiv(DV, BV)):
+        # [BT, BV]
+        b_v = tl.load(p_v, boundary_check=(0, 1))
+        b_do = tl.load(p_do, boundary_check=(0, 1))
+        # [BV, BK]
+        b_h = tl.load(p_h, boundary_check=(0, 1))
+        # [BK, BV]
+        b_dh = tl.load(p_dh, boundary_check=(0, 1))
 
-            # [BT, BT]
-            b_ds = tl.dot(b_do, tl.trans(b_v), allow_tf32=False)
-            b_ds = (b_ds * d_s).to(b_k.dtype)
-            # [BT, BK]
-            b_dq += tl.dot(b_do, b_h, allow_tf32=False) * d_q[:, None] + tl.dot(b_ds, b_k, allow_tf32=False)
+        # [BT, BT]
+        b_ds = tl.dot(b_do, tl.trans(b_v), allow_tf32=False)
+        b_ds = (b_ds * d_s).to(b_k.dtype)
+        # [BT, BK]
+        b_dq += tl.dot(b_do, b_h, allow_tf32=False) * d_q[:, None] + tl.dot(b_ds, b_k, allow_tf32=False)
 
-            # [BT, BT]
-            b_ds = tl.trans(b_ds)
-            # [BK, BT]
-            b_dk += tl.dot(b_v, tl.trans(b_dh), allow_tf32=False) * d_k[:, None]
-            b_dk += tl.dot(b_ds, tl.trans(b_q), allow_tf32=False)
-            b_dv = tl.dot(b_k, b_dh, allow_tf32=False) * d_k[:, None] + tl.dot(b_s.to(b_q.dtype), b_do, allow_tf32=False)
-            b_dv += tl.load(p_dv, boundary_check=(0, 1)).to(tl.float32)
-            tl.store(p_dv, b_dv.to(p_dv.dtype.element_ty), boundary_check=(0, 1))
+        # [BT, BT]
+        b_ds = tl.trans(b_ds)
+        # [BK, BT]
+        b_dk += tl.dot(b_v, tl.trans(b_dh), allow_tf32=False) * d_k[:, None]
+        b_dk += tl.dot(b_ds, tl.trans(b_q), allow_tf32=False)
+        b_dv = tl.dot(b_k, b_dh, allow_tf32=False) * d_k[:, None] + tl.dot(b_s.to(b_q.dtype), b_do, allow_tf32=False)
+        tl.store(p_dv, b_dv.to(p_dv.dtype.element_ty), boundary_check=(0, 1))
 
-            p_v = tl.advance(p_v, (0, BV))
-            p_h = tl.advance(p_h, (BV, 0))
-            p_do = tl.advance(p_do, (0, BV))
-            p_dh = tl.advance(p_dh, (0, BV))
-            p_dv = tl.advance(p_dv, (0, BV))
+        p_v = tl.advance(p_v, (0, BV))
+        p_h = tl.advance(p_h, (BV, 0))
+        p_do = tl.advance(p_do, (0, BV))
+        p_dh = tl.advance(p_dh, (0, BV))
+        p_dv = tl.advance(p_dv, (0, BV))
 
-        tl.store(p_dq, b_dq.to(p_dq.dtype.element_ty), boundary_check=(0, 1))
-        tl.store(p_dk, b_dk.to(p_dk.dtype.element_ty), boundary_check=(0, 1))
+    tl.store(p_dq, b_dq.to(p_dq.dtype.element_ty), boundary_check=(0, 1))
+    tl.store(p_dk, b_dk.to(p_dk.dtype.element_ty), boundary_check=(0, 1))
 
 
 class ChunkRetentionFunction(torch.autograd.Function):
@@ -353,11 +352,11 @@ class ChunkRetentionFunction(torch.autograd.Function):
 
         BK, BV = min(64, triton.next_power_of_2(DK)), min(64, triton.next_power_of_2(DV))
         NK, NV = triton.cdiv(DK, BK), triton.cdiv(DV, BV)
-        grid = (triton.cdiv(seq_len, BT), batch_size * n_heads)
+        grid = (NK, triton.cdiv(seq_len, BT), batch_size * n_heads)
         dq = torch.empty_like(q)
         dk = torch.empty_like(k)
         # must be zero. we need reload
-        dv = torch.zeros_like(v)
+        dv = v.new_empty(NK, *v.shape)
         num_stages = 1
         num_warps = 4 if BK == 64 else 2
         chunk_retention_bwd_kernel_dqkv[grid](
@@ -370,6 +369,7 @@ class ChunkRetentionFunction(torch.autograd.Function):
             num_warps=num_warps,
             num_stages=num_stages
         )
+        dv = dv.sum(0)
         return dq.to(q.dtype), dk.to(k.dtype), dv.to(v.dtype), None, None
 
 
