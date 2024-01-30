815f450c29d84a59b94a7bd2f24d23ec0a9a775c
yzhangcs <yzhang.cs@outlook.com>
2024-01-31 02:26:46 +0800

    Slight revise for safer dk

--- fla/ops/abc/chunk.py
+++ fla/ops/abc/chunk.py
@@ -313,15 +313,6 @@ def chunk_abc_bwd_kernel_dqkv(
 
     p_q = tl.make_block_ptr(q + i_bh * s_k_h, (K, T), (s_k_d, s_k_t), (i_k * BK, i_t * BT), (BK, BT), (0, 1))
     p_k = tl.make_block_ptr(k + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
-    p_v = tl.make_block_ptr(v + i_bh * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, 0), (BT, BV), (1, 0))
-    p_h = tl.make_block_ptr(h + i_bh * s_h_h, (V, (i_t+1)*K), (s_h_d, s_h_t), (0, i_t * K + i_k * BK), (BV, BK), (0, 1))
-
-    p_do = tl.make_block_ptr(do + i_bh * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, 0), (BT, BV), (1, 0))
-    p_dh = tl.make_block_ptr(dh + i_bh * s_h_h, ((i_t+1)*K, V), (s_h_t, s_h_d), (i_t * K + i_k * BK, 0), (BK, BV), (1, 0))
-
-    p_dq = tl.make_block_ptr(dq + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
-    p_dk = tl.make_block_ptr(dk + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
-    p_dv = tl.make_block_ptr(dv + (i_k * n_bh + i_bh) * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, 0), (BT, BV), (1, 0))
 
     b_m = tl.load(m + i_bh * NT + i_t).to(tl.float32)
     # [BK, BT]
@@ -340,6 +331,12 @@ def chunk_abc_bwd_kernel_dqkv(
     b_dq = tl.zeros([BT, BK], dtype=tl.float32)
     b_dk = tl.zeros([BT, BK], dtype=tl.float32)
     for i_v in range(tl.cdiv(V, BV)):
+        p_v = tl.make_block_ptr(v + i_bh * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, i_v * BV), (BT, BV), (1, 0))
+        p_h = tl.make_block_ptr(h + i_bh * s_h_h, (V, (i_t+1)*K), (s_h_d, s_h_t), (i_v * BV, i_t*K+i_k*BK), (BV, BK), (0, 1))
+        p_do = tl.make_block_ptr(do + i_bh * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, i_v * BV), (BT, BV), (1, 0))
+        p_dh = tl.make_block_ptr(dh + i_bh * s_h_h, ((i_t+1)*K, V), (s_h_t, s_h_d), (i_t*K+i_k*BK, i_v * BV), (BK, BV), (1, 0))
+        p_dv = tl.make_block_ptr(dv + (i_k*n_bh+i_bh) * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, i_v * BV), (BT, BV), (1, 0))
+
         # [BT, BV]
         b_v = tl.load(p_v, boundary_check=(0, 1))
         # [BV, BK]
@@ -365,17 +362,16 @@ def chunk_abc_bwd_kernel_dqkv(
         # [BT, BT]
         b_ds = tl.trans(b_ds)
         # [BT, BK]
-        b_dk += tl.dot(b_v, tl.trans(b_dh), allow_tf32=False) + safe_exy(b_m, tl.dot(b_ds, tl.trans(b_q), allow_tf32=False))
         b_dv = tl.dot(b_k, b_dh, allow_tf32=False) + tl.dot(b_s, b_do, allow_tf32=False)
-        if not NORMQ:
+        # the rescale term m cancels the denominator of either v or k out, so in general dk is safe
+        if NORMQ:
+            b_dk += tl.dot(b_v, tl.trans(b_dh), allow_tf32=False) * b_k
+            b_dk += safe_exy(b_m, tl.dot(b_ds, tl.trans(b_q), allow_tf32=False) * b_k)
+        else:
+            b_dk += tl.dot(b_v, tl.trans(b_dh), allow_tf32=False)
+            b_dk += safe_exy(b_m, tl.dot(b_ds, tl.trans(b_q), allow_tf32=False))
             b_dv = b_v * b_dv
         tl.store(p_dv, b_dv.to(p_dv.dtype.element_ty), boundary_check=(0, 1))
-
-        p_v = tl.advance(p_v, (0, BV))
-        p_h = tl.advance(p_h, (BV, 0))
-        p_do = tl.advance(p_do, (0, BV))
-        p_dh = tl.advance(p_dh, (0, BV))
-        p_dv = tl.advance(p_dv, (0, BV))
 
     if NORMQ:
         p_z = tl.make_block_ptr(z + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
@@ -383,8 +379,10 @@ def chunk_abc_bwd_kernel_dqkv(
         b_z = tl.load(p_z, boundary_check=(0, 1)).to(tl.float32)
         b_r = tl.load(p_r, boundary_check=(0,)).to(tl.float32)
         b_dq = b_dq * b_z * b_r[None, :]
-        b_dk = b_dk * b_k
     b_dq = safe_exy(b_m, b_dq)
+
+    p_dq = tl.make_block_ptr(dq + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
+    p_dk = tl.make_block_ptr(dk + i_bh * s_k_h, (T, K), (s_k_t, s_k_d), (i_t * BT, i_k * BK), (BT, BK), (1, 0))
     tl.store(p_dq, b_dq.to(p_dq.dtype.element_ty), boundary_check=(0, 1))
     tl.store(p_dk, b_dk.to(p_dk.dtype.element_ty), boundary_check=(0, 1))
 
