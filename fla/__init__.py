98847879b9de46d7d16e9e56c1d50cdb107090f4
yzhangcs <yzhang.cs@outlook.com>
2024-02-16 21:04:39 +0800

    Update available utilities

--- fla/__init__.py
+++ fla/__init__.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from fla.ops import chunk_gla, chunk_retention, fused_chunk_based
+from fla.layers import (BasedLinearAttention, GatedLinearAttention,
+                        MultiScaleRetention)
+from fla.models import GLAForCausalLM, GLAModel, RetNetForCausalLM, RetNetModel
+from fla.ops import (chunk_gla, chunk_retention, fused_chunk_based,
+                     fused_chunk_gla, fused_chunk_retention)
 
 __all__ = [
+    'BasedLinearAttention',
+    'GatedLinearAttention',
+    'MultiScaleRetention',
+    'GLAForCausalLM',
+    'GLAModel',
+    'RetNetForCausalLM',
+    'RetNetModel',
     'chunk_gla',
     'chunk_retention',
-    'fused_chunk_based'
+    'fused_chunk_based',
+    'fused_chunk_gla',
+    'fused_chunk_retention'
 ]
 
 __version__ = '0.1'
