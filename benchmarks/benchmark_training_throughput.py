72c2a17c8c9a71387648b37ae8d2083498f2a202
yzhangcs <yzhang.cs@outlook.com>
2024-03-12 13:45:17 +0800

    Update thoughput benchmarks

--- benchmarks/benchmark_training_throughput.py
+++ benchmarks/benchmark_training_throughput.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import time
 
 import torch
-from torch.cuda import max_memory_allocated
+from torch.cuda import max_memory_allocated, memory_allocated
 from torch.optim import AdamW
-from tqdm import tqdm
+from tqdm import trange
 from transformers import AutoModelForCausalLM
 
-from fla.models.abc import ABCConfig, ABCForCausalLM
-from fla.models.delta_net import DeltaNetConfig, DeltaNetForCausalLM
-from fla.models.gla import GLAConfig, GLAForCausalLM
-from fla.models.linear_attn import (LinearAttentionConfig,
-                                    LinearAttentionForCausalLM)
-from fla.models.retnet import RetNetConfig, RetNetForCausalLM
-import time
+from fla.models.abc import ABCConfig
+from fla.models.delta_net import DeltaNetConfig
+from fla.models.gla import GLAConfig
+from fla.models.linear_attn import LinearAttentionConfig
+from fla.models.retnet import RetNetConfig
 
 configs = {
     'abc': ABCConfig,
@@ -33,19 +32,19 @@ def sizeof_fmt(num, suffix='B'):
     return f'{num:.1f}Yi{suffix}'
 
 
-def profile_throughput(name: str, batch_size: int = 8, seq_len: int = 2048, warmup_steps: int = 5, steps: int=10):
+def profile_throughput(name: str, batch_size: int = 8, seq_len: int = 2048, warmup_steps: int = 16, steps: int = 32):
     device = torch.device('cuda')
     config = configs[name]()
     model = AutoModelForCausalLM.from_config(config).cuda().to(torch.bfloat16)
     print(f"Initializing {name} model from the config")
     print(config)
     print(model)
-    print(f"Allocated memory after initialization: {sizeof_fmt(max_memory_allocated(device))}")
+    print(f"Allocated memory after initialization: {sizeof_fmt(memory_allocated(device))}")
 
     optimizer = AdamW(model.parameters())
 
-    for _ in tqdm(range(warmup_steps)):
-        
+    bar = trange(warmup_steps)
+    for _ in bar:
         # forward pass
         tokens = torch.randint(high=config.vocab_size, size=(batch_size, seq_len)).cuda()
         outputs = model(tokens, labels=tokens)
@@ -53,11 +52,13 @@ def profile_throughput(name: str, batch_size: int = 8, seq_len: int = 2048, warm
         outputs.loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-    
-    print(f"Max memory allocated: {sizeof_fmt(max_memory_allocated(device))}")
+        bar.set_description_str(f"Max memory allocated: {sizeof_fmt(max_memory_allocated(device))}")
+
     torch.cuda.synchronize(device)
-    cur_time = time.time()
-    for _ in tqdm(range(steps)):
+    start = time.time()
+    bar = trange(steps)
+    total_tokens = 0
+    for _ in bar:
         # forward pass
         tokens = torch.randint(high=config.vocab_size, size=(batch_size, seq_len), device=device)
         outputs = model(tokens, labels=tokens)
@@ -65,19 +66,18 @@ def profile_throughput(name: str, batch_size: int = 8, seq_len: int = 2048, warm
         outputs.loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-    torch.cuda.synchronize(device)
-    duration = time.time() - cur_time
-    num_tokens = batch_size * seq_len * steps
-    tokens_per_sec = num_tokens / duration
-    print(f"Thoughput: {tokens_per_sec}")
-    
+
+        total_tokens += batch_size * seq_len
+        duration = time.time() - start
+        bar.set_description_str(f"Thoughput: {total_tokens / duration:8.2f} tokens/s")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--name", default='delta_net')
+    parser.add_argument("--name", default='retnet')
     parser.add_argument("--batch_size", default=4, type=int)
     parser.add_argument("--seq_len", default=2048, type=int)
-    parser.add_argument("--warmup_steps", default=10, type=int)
-    parser.add_argument("--steps", default=20, type=int)
+    parser.add_argument("--warmup_steps", default=16, type=int)
+    parser.add_argument("--steps", default=32, type=int)
     args = parser.parse_args()
     profile_throughput(args.name, args.batch_size, args.seq_len, args.warmup_steps, args.steps)
