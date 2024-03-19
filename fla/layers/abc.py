d05f86d6dbc8cc89654b83137060f8567113d77a
yzhangcs <yzhang.cs@outlook.com>
2024-03-20 03:49:29 +0800

    Short conv supported

--- fla/layers/abc.py
+++ fla/layers/abc.py
@@ -10,7 +10,9 @@ import torch.nn as nn
 from einops import rearrange
 from transformers.cache_utils import Cache
 
-from fla.modules import FusedRMSNormSwishGate, RotaryEmbedding
+from fla.layers.utils import proj_then_conv1d
+from fla.modules import (FusedRMSNormSwishGate, RotaryEmbedding,
+                         ShortConvolution)
 from fla.ops.abc.chunk import chunk_abc
 
 
@@ -22,6 +24,10 @@ class ABCAttention(nn.Module):
         expand_k: float = 1.0,
         expand_v: float = 1.0,
         num_heads: int = 4,
+        use_short_conv: bool = True,
+        conv_size: int = 4,
+        conv_bias: bool = False,
+        share_conv_kernel: bool = True,
         num_slots: Optional[int] = None,
         layernorm_eps: float = 1e-5,
         gate_low_rank_dim: int = 16,
@@ -37,9 +43,9 @@ class ABCAttention(nn.Module):
         self.expand_v = expand_v
         self.num_heads = num_heads
         self.key_dim = int(self.hidden_size * self.expand_k)
-        self.val_dim = int(self.hidden_size * self.expand_v)
+        self.value_dim = int(self.hidden_size * self.expand_v)
         self.head_k_dim = self.key_dim // self.num_heads
-        self.head_v_dim = self.val_dim // self.num_heads
+        self.head_v_dim = self.value_dim // self.num_heads
 
         if num_slots is None:
             num_slots = self.head_k_dim
@@ -59,9 +65,18 @@ class ABCAttention(nn.Module):
 
         self.q_proj = nn.Linear(self.hidden_size, self.key_dim, bias=False)
         self.k_proj = nn.Linear(self.hidden_size, self.key_dim, bias=False)
-        self.v_proj = nn.Linear(self.hidden_size, self.val_dim, bias=False)
-        self.g_proj = nn.Linear(self.hidden_size, self.val_dim, bias=False)
-        self.o_proj = nn.Linear(self.hidden_size, self.val_dim, bias=False)
+        self.v_proj = nn.Linear(self.hidden_size, self.value_dim, bias=False)
+        self.g_proj = nn.Linear(self.hidden_size, self.value_dim, bias=False)
+        self.o_proj = nn.Linear(self.value_dim, self.hidden_size, bias=False)
+
+        if use_short_conv:
+            self.conv_size = conv_size
+            if share_conv_kernel:
+                self.h_conv1d = ShortConvolution(hidden_size, conv_size, activation='silu')
+            else:
+                self.q_conv1d = ShortConvolution(self.key_dim, conv_size, activation='silu')
+                self.k_conv1d = ShortConvolution(self.key_dim, conv_size, activation='silu')
+                self.v_conv1d = ShortConvolution(self.value_dim, conv_size, activation='silu')
 
         self.g_norm = FusedRMSNormSwishGate(self.head_v_dim)
         self.s_proj = nn.Sequential(
@@ -90,15 +105,32 @@ class ABCAttention(nn.Module):
         output_attentions: Optional[bool] = False,
         **kwargs
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Cache]]:
-        q = rearrange(self.q_proj(hidden_states), '... (h d) -> ... h d', h=self.num_heads)
-        k = rearrange(self.k_proj(hidden_states), '... (h d) -> ... h d', h=self.num_heads)
+
+        if self.use_short_conv:
+            if self.share_conv_kernel:
+                hidden_states = self.h_conv1d(hidden_states)
+                q = self.q_proj(hidden_states)
+                k = self.k_proj(hidden_states)
+                v = self.v_proj(hidden_states)
+            else:
+                q = proj_then_conv1d(hidden_states, self.q_proj.weight, self.q_conv1d.weight, self.q_conv1d.bias)
+                k = proj_then_conv1d(hidden_states, self.k_proj.weight, self.k_conv1d.weight, self.k_conv1d.bias)
+                v = proj_then_conv1d(hidden_states, self.v_proj.weight, self.v_conv1d.weight, self.v_conv1d.bias)
+        else:
+            q = self.q_proj(hidden_states)
+            k = self.k_proj(hidden_states)
+            v = self.v_proj(hidden_states)
+
+        q = rearrange(q, '... (h d) -> ... h d', h=self.num_heads)
+        k = rearrange(k, '... (h d) -> ... h d', h=self.num_heads)
+        v = rearrange(v, 'b n (h d) -> b h n d', h=self.num_heads)
 
         seqlen_offset = 0
         if past_key_values is not None:
             seqlen_offset = past_key_values.get_seq_length()
         q, k = self.rotary(q, k, seqlen_offset)
         q, k = q.transpose(1, 2), k.transpose(1, 2)
-        v = rearrange(self.v_proj(hidden_states), 'b n (h d) -> b h n d', h=self.num_heads)
+
         # [batch_size, n_heads, seq_len, num_slots]
         s = rearrange(self.s_proj(hidden_states), 'b t (h m) -> b h t m', h=self.num_heads)
         s = s.clamp_(self.clamp_min, self.clamp_max)
