414bf8dd132095f3fa566b4aa28ecb8bb0927f1b
yzhangcs <yzhang.cs@outlook.com>
2024-03-22 21:15:38 +0800

    Add cache support

--- fla/layers/utils.py
+++ fla/layers/utils.py
@@ -1,37 +1,47 @@
-import torch.nn as nn
+# -*- coding: utf-8 -*-
+
+from typing import Optional
+
+import torch
 from einops import rearrange
 
 from fla.modules.utils import checkpoint
 
 try:
-    from causal_conv1d import causal_conv1d_fn
+    from causal_conv1d import causal_conv1d_fn, causal_conv1d_update
 except ImportError:
     causal_conv1d_fn = None
     causal_conv1d_update = None
 
 
 @checkpoint
-def proj_then_conv1d(x, proj_weight, conv1d_weight, conv1d_bias):
+def proj_then_conv1d(
+    x: torch.Tensor,
+    proj_weight: torch.Tensor,
+    conv1d_weight: torch.Tensor,
+    conv1d_bias: Optional[torch.Tensor] = None,
+    cache: Optional[torch.Tensor] = None
+) -> torch.Tensor:
     # We do matmul and transpose BLH -> HBL at the same time
     x = rearrange(proj_weight @ rearrange(x, "b l d -> d (b l)"), "d (b l) -> b d l", l=x.shape[-2])
 
-    def conv1d(x, conv1d_weight, conv1d_bias):
-        return causal_conv1d_fn(
+    if causal_conv1d_fn is None:
+        raise ImportError("`causal_conv1d_fn` is not available. Please install `causal-conv1d` first.")
+    if cache is None:
+        x = causal_conv1d_fn(
             x=x,
             weight=rearrange(conv1d_weight, "d 1 w -> d w"),
             bias=conv1d_bias,
             activation="silu",
+        ).transpose(1, 2)
+    else:
+        assert x.shape[-1] == 1, "Only support decoding with 1 token at a time for now"
+        x = x.squeeze(-1)
+        x = causal_conv1d_update(
+            x=x,
+            weight=rearrange(conv1d_weight, "d 1 w -> d w"),
+            bias=conv1d_bias,
+            cache=cache,
+            activation="silu",
         )
-    y = conv1d(x, conv1d_weight, conv1d_bias)
-    return y.transpose(1, 2)
-
-
-def make_conv1d_module(hidden_size, conv_size, use_bias=False):
-    return nn.Conv1d(
-        in_channels=hidden_size,
-        out_channels=hidden_size,
-        kernel_size=conv_size,
-        groups=hidden_size,
-        padding=conv_size-1,
-        bias=use_bias,
-    )
+    return x
