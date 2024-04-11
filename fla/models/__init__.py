681aebe9daba05f7d46014bf090054b1e458cf6b
yzhangcs <yzhang.cs@outlook.com>
2024-04-12 01:47:31 +0800

    Add HGRN2

--- fla/models/__init__.py
+++ fla/models/__init__.py
@@ -4,6 +4,7 @@ from fla.models.abc import ABCConfig, ABCForCausalLM, ABCModel
 from fla.models.delta_net import (DeltaNetConfig, DeltaNetForCausalLM,
                                   DeltaNetModel)
 from fla.models.gla import GLAConfig, GLAForCausalLM, GLAModel
+from fla.models.hgrn2 import HGRN2Config, HGRN2ForCausalLM, HGRN2Model
 from fla.models.linear_attn import (LinearAttentionConfig,
                                     LinearAttentionForCausalLM,
                                     LinearAttentionModel)
@@ -15,6 +16,7 @@ __all__ = [
     'ABCConfig', 'ABCForCausalLM', 'ABCModel',
     'DeltaNetConfig', 'DeltaNetForCausalLM', 'DeltaNetModel',
     'GLAConfig', 'GLAForCausalLM', 'GLAModel',
+    'HGRN2Config', 'HGRN2ForCausalLM', 'HGRN2Model',
     'LinearAttentionConfig', 'LinearAttentionForCausalLM', 'LinearAttentionModel',
     'RetNetConfig', 'RetNetForCausalLM', 'RetNetModel',
     'TransformerConfig', 'TransformerForCausalLM', 'TransformerModel'
