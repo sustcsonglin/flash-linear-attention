f4fbb7c3c2ffe818bd68c77ab1a872100ff35ffd
Yu Zhang <yzhang.cs@outlook.com>
2024-04-19 05:12:20 +0800

    [Activations] Add sigmoid

--- fla/modules/activations.py
+++ fla/modules/activations.py
@@ -5,6 +5,37 @@
 import torch
 import torch.nn.functional as F
 
+sigmoid_fwd_codestring = """
+template <typename T> T sigmoid_fwd(T x) {
+    return 1.0f / (1.0f + ::exp(-float(x)));
+}
+"""
+sigmoid_bwd_codestring = """
+template <typename T> T sigmoid_bwd(T x, T g) {
+    float x_sigmoid = 1.0f / (1.0f + ::exp(-float(x)));
+    return float(g) * x_sigmoid * (1.0f - x_sigmoid);
+}
+"""
+
+sigmoid_fwd = torch.cuda.jiterator._create_jit_fn(sigmoid_fwd_codestring)
+sigmoid_bwd = torch.cuda.jiterator._create_jit_fn(sigmoid_bwd_codestring)
+
+
+class SigmoidFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        return sigmoid_fwd(x)
+
+    @staticmethod
+    def backward(ctx, dout):
+        x, = ctx.saved_tensors
+        return sigmoid_bwd(x, dout)
+
+
+sigmoid = SigmoidFunction.apply
+
 swish_fwd_codestring = """
 template <typename T> T swish_fwd(T x) {
     float x_sigmoid = 1.0f / (1.0f + ::exp(-float(x)));
@@ -237,6 +268,7 @@ swiglu = SwiGLUFunction.apply
 swiglu_linear = SwiGLULinearFunction.apply
 
 ACT2FN = {
+    'sigmoid': sigmoid,
     'silu': swish,
     'swish': swish,
     'sqrelu': sqrelu,
