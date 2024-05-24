[ABC] Fix scaling factor

fla/ops/abc/naive.py
@@ -10,47 +10,51 @@ def naive_recurrent_abc(
     k: torch.Tensor,
     v: torch.Tensor,
     s: torch.Tensor,
+    g: Optional[torch.Tensor] = None,
+    scale: Optional[int] = None,
     initial_state: Optional[torch.Tensor] = None,
     output_final_state: Optional[bool] = False
 ) -> torch.Tensor:
     dtype = q.dtype
-    q, k, v, s = map(lambda x: x.float(), (q, k, v, s))
+
     # [batch_size, n_heads, seq_len, n_slots]
-    z = s.logcumsumexp(2)
-    g = torch.cat((z[:, :, :1], z[:, :, :-1]), 2) - z
+    if g is None:
+        z = s.float().logcumsumexp(2)
+        g = torch.cat((z[:, :, :1], z[:, :, :-1]), 2) - z
+        s = torch.exp(s - z)
+    q, k, v, s, g = map(lambda x: x.float(), (q, k, v, s, g))
     B, H, T, K, V, M = *q.shape, v.shape[-1], s.shape[-1]
 
     hk = torch.zeros(B, H, K, M, dtype=torch.float, device=q.device)
     ok = torch.zeros_like(s)
-    scale = K ** -0.5
+
+    if scale is None:
+        scale = q.shape[-1] ** -0.5
 
     final_state = None
     if initial_state is not None:
-        hk += initial_state[0].detach()
+        hk += initial_state[0]
 
     for i in range(T):
         q_i = q[:, :, i] * scale
         k_i = k[:, :, i]
         v_i = s[:, :, i]
-        z_i = z[:, :, i]
         g_i = g[:, :, i].exp()
-        hk = hk * g_i[..., None, :] + k_i[..., None] * (v_i - z_i).exp()[..., None, :]
+        hk = hk * g_i[..., None, :] + k_i[..., None] * v_i[..., None, :]
         ok[:, :, i] = (q_i[..., None] * hk).sum(-2)
-    p = ok.softmax(-1)
 
+    qv = ok.softmax(-1)
     hv = torch.zeros(B, H, M, V, dtype=torch.float, device=q.device)
     ov = torch.zeros_like(v)
-
     if initial_state is not None:
-        hv += initial_state[1].detach()
+        hv += initial_state[1]
 
     for i in range(T):
-        q_i = p[:, :, i]
+        q_i = qv[:, :, i]
         k_i = s[:, :, i]
         v_i = v[:, :, i]
-        z_i = z[:, :, i]
         g_i = g[:, :, i].exp()
-        hv = hv * g_i[..., :, None] + (k_i - z_i).exp()[..., None] * v_i[..., None, :]
+        hv = hv * g_i[..., :, None] + k_i[..., None] * v_i[..., None, :]
         ov[:, :, i] = (q_i[..., None] * hv).sum(-2)
 
     if output_final_state:
@@ -66,7 +70,7 @@ def naive_cumsum_abc(
 ) -> torch.Tensor:
     """
     A simple implementation of vanilla ABC that is more aligned with the descriptions in the paper.
-    This is just for demonstration purposes, with no numerical stabilities gaurenteed.
+    This is just for demonstration purposes, with no numerical stabilities guaranteed.
     """
 
     dtype = q.dtype