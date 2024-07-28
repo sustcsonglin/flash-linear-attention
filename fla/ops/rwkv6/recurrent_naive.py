31290d15e6bfafb4c2c434e56d1854f833183eb5
Zhiyuan Li <uniartisan2017@gmail.com>
2024-07-29 02:29:00 +1000

    fix: calculate du on different batch

--- fla/ops/rwkv6/recurrent_naive.py
+++ fla/ops/rwkv6/recurrent_naive.py
@@ -39,20 +39,21 @@ def naive_recurrent_rwkv6(
     ht = h if output_final_state else None
     return o.to(orig_dtype), ht
 
-
+@torch.no_grad
+@torch.jit.script
 def naive_recurrent_rwkv6_bwd(
-    q,
-    k,
-    v,
-    w,
-    u,
-    o,
-    do,
-    initial_state=None,
-    output_final_state=False
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    w: torch.Tensor,
+    u: torch.Tensor,
+    o: torch.Tensor,
+    do: torch.Tensor,
+    initial_state: Optional[torch.Tensor] = None,
+    output_final_state: bool = False
 ):
-    q, k, v, w, u, o, do = map(lambda x: x.float(), (q, k, v, w, u, o, do))
-    B, H, T, K, V = *q.shape, v.shape[-1]
+    q, k, v, w, u, o, do = (x.to(dtype=torch.float32) for x in (q, k, v, w, u, o, do))
+    B, H, T, K, V = q.shape[0], q.shape[1], q.shape[2], q.shape[3], v.shape[-1]
     h = torch.zeros(B, H, K, V, dtype=torch.float32, device=q.device)
     dq = torch.zeros_like(q)
     dq_aux = torch.zeros_like(q)
@@ -83,7 +84,7 @@ def naive_recurrent_rwkv6_bwd(
         k_i = k[:, :, i]
         v_i = v[:, :, i]
         du_i = (d_kv_i * k_i[..., None] * v_i[..., None, :]).sum(-1)
-        du += du_i
+        du += du_i.sum(0)
         dk_i = (dh * v_i[..., None, :]).sum(-1)
         dk_aux[:, :, i] = dk_i
         dk_i += (d_kv_i * u[None, ..., None] * v_i[..., None, :]).sum(-1)
@@ -99,4 +100,37 @@ def naive_recurrent_rwkv6_bwd(
     for i in range(T - 2, -1, -1):
         dw[:, :, i] = dw[:, :, i+1] + dq_aux[:, :, i+1] * q[:, :, i+1] - dk_aux[:, :, i] * k[:, :, i]
 
-    return dq, dk, dv, dw, du
+    return dq, dk, dv, dw, du, dh
+
+if __name__ == "__main__":
+    device = "cuda"
+    B = 4
+    H = 4
+    L = 1024
+    D = 100
+    dtype = torch.float
+    require_grad = True
+    q = (torch.randn(B, H, L, D).to(device).to(dtype)).requires_grad_(require_grad)
+    k = (torch.randn(B, H, L, D).to(device).to(dtype)).requires_grad_(require_grad)
+    v = torch.randn(B, H, L, 2*D).to(device).to(dtype).requires_grad_(require_grad)
+    w = torch.nn.functional.logsigmoid(torch.randn(B, H, L, D)).to(device).to(dtype).requires_grad_(require_grad)
+    u = (torch.randn(H, D).to(device).to(dtype)).requires_grad_(require_grad)
+    do = torch.rand_like(v).to(device)
+    h = torch.randn(B, H, D, 2*D, device=device, dtype=torch.float32, requires_grad=True)
+    o, _ = naive_recurrent_rwkv6(q, k, v, w, u, scale=1.0, initial_state=h)
+    o.backward(do)
+    dq, q.grad = q.grad.clone(), None
+    dk, k.grad = k.grad.clone(), None
+    dv, v.grad = v.grad.clone(), None
+    dw, w.grad = w.grad.clone(), None
+    du, u.grad = u.grad.clone(), None
+    dh, h.grad = h.grad.clone(), None
+
+    dq2, dk2, dv2, dw2, du2, dh2 = naive_recurrent_rwkv6_bwd(q, k, v, w, u, o, do, initial_state=h)
+
+    assert torch.allclose(dq, dq2, atol=1e-3)
+    assert torch.allclose(dk, dk2, atol=1e-3)
+    assert torch.allclose(dv, dv2, atol=1e-3)
+    assert torch.allclose(dw, dw2, atol=1e-3)
+    assert torch.allclose(du, du2, atol=1e-3)
+    assert torch.allclose(dh, dh2, atol=1e-3)
