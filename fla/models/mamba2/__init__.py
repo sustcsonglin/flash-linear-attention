fd66250173b3de66b48c61f090a4b6534d28b19b
DanFosing <65554751+DanFosing@users.noreply.github.com>
2024-08-05 15:44:24 +0200

    Add __init__.py

---
+++ fla/models/mamba2/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
+
+from fla.models.mamba2.configuration_mamba2 import Mamba2Config
+from fla.models.mamba2.modeling_mamba2 import (Mamba2Block, Mamba2ForCausalLM,
+                                             Mamba2Model)
+
+AutoConfig.register(Mamba2Config.model_type, Mamba2Config, True)
+AutoModel.register(Mamba2Config, Mamba2Model, True)
+AutoModelForCausalLM.register(Mamba2Config, Mamba2ForCausalLM, True)
+
+
+__all__ = ['Mamba2Config', 'Mamba2ForCausalLM', 'Mamba2Model', 'Mamba2Block']
