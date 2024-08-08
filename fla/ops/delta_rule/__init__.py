9993697b312fadb75bd5bdc3ac90d49f44350640
hypnopump <ericalcaide1@gmail.com>
2024-08-08 11:39:36 +0200

    improve naming

--- fla/ops/delta_rule/__init__.py
+++ fla/ops/delta_rule/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from .recurrent_fuse import fused_recurrent_linear_attn_delta_rule
+from .recurrent_fuse_betavec import fused_recurrent_linear_attn_delta_rule_betavec
+
 # from .chunk_fn import chunk_linear_attn_delta_rule
 from .chunk_fused import fused_chunk_delta_rule
 
 __all__ = [
     'fused_recurrent_linear_attn_delta_rule',
-    'fused_chunk_delta_rule'
+    'fused_chunk_delta_rule',
+    'fused_recurrent_linear_attn_delta_rule_betavec'
 ]
 
