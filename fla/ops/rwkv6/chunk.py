[rwkv6]: fix gradient error due to num_wraps

fla/ops/rwkv6/chunk.py
@@ -62,17 +62,7 @@ def chunk_rwkv6_fwd_kernel_cum(
     tl.store(p_o_minus_s, (b_o - b_s).to(p_o_minus_s.dtype.element_ty), boundary_check=(0, 1))
 
 
-@triton.autotune(
-    configs=[
-        triton.Config({}, num_warps=2, num_stages=1),
-        triton.Config({}, num_warps=4, num_stages=1),
-        triton.Config({}, num_warps=8, num_stages=1),
-        triton.Config({}, num_warps=2, num_stages=2),
-        triton.Config({}, num_warps=4, num_stages=2),
-        triton.Config({}, num_warps=8, num_stages=2),
-    ],
-    key=['BK']
-)
+
 @triton.jit
 def post_process_grad(
     q,
@@ -110,11 +100,11 @@ def post_process_grad(
     p_do = tl.make_block_ptr(do + i_bh * s_v_h, (T, V), (s_v_t, s_v_d), (i_t * BT, 0), (BT, BV), (1, 0))
     p_u = tl.make_block_ptr(u + i_bh * K, (K,), (1,), (0,), (BK,), (0,))
 
-    b_q = tl.load(p_q, boundary_check=(0, 1))
-    b_k = tl.load(p_k, boundary_check=(0, 1))
-    b_v = tl.load(p_v, boundary_check=(0, 1))
-    b_do = tl.load(p_do, boundary_check=(0, 1))
-    b_u = tl.load(p_u, boundary_check=(0,))
+    b_q = tl.load(p_q, boundary_check=(0, 1)).to(tl.float32)
+    b_k = tl.load(p_k, boundary_check=(0, 1)).to(tl.float32)
+    b_v = tl.load(p_v, boundary_check=(0, 1)).to(tl.float32)
+    b_do = tl.load(p_do, boundary_check=(0, 1)).to(tl.float32)
+    b_u = tl.load(p_u, boundary_check=(0,)).to(tl.float32)
 
     b_vdo = tl.sum(b_v * b_do, axis=1)
     b_du = b_vdo[:, None] * b_k * b_q * scale
@@ -572,15 +562,6 @@ def chunk_rwkv6_bwd_kernel_inter(
         tl.store(p_dA, b_dA.to(p_dA.dtype.element_ty), boundary_check=(0, 1))
 
 
-@triton.autotune(
-    configs=[
-        triton.Config({}, num_warps=2, num_stages=1),
-        triton.Config({}, num_warps=4, num_stages=1),
-        triton.Config({}, num_warps=8, num_stages=1),
-        triton.Config({}, num_warps=16, num_stages=1),
-    ],
-    key=['BK']
-)
 @triton.jit
 def chunk_rwkv6_bwd_kernel_intra(
     q,
@@ -763,14 +744,14 @@ class ChunkRWKV6Function(torch.autograd.Function):
             del h
             h, initial_state = None, None
         else:
-            h_t = h
+            h_t, initial_state = h, initial_state.clone()
         del g, gs
         if training:
-            ctx.save_for_backward(q, k, v, g_org, u, h_t, initial_state.clone(), A)
+            ctx.save_for_backward(q, k, v, g_org, u, h_t, initial_state, A)
             ctx.BT = BT
             ctx.scale = scale
             ctx.checkpoint_level = checkpoint_level
-        return o, final_state if final_state is not None else None
+        return o, final_state
 
     @staticmethod
     @contiguous
@@ -787,6 +768,8 @@ class ChunkRWKV6Function(torch.autograd.Function):
         NT, NC = triton.cdiv(T, BT), triton.cdiv(BT, BC)
         NK = triton.cdiv(K, BK)
         BH = B * H
+        num_warps = 4 if BK == 64 else 2
+        num_stages = 1
 
         def fwd_inner(q, k, v, g, B, H, T, K, V, BT, BK, BV, NT, h, h0=None, ht=None):
             NK, NV = triton.cdiv(K, BK), triton.cdiv(V, BV)
@@ -859,6 +842,8 @@ class ChunkRWKV6Function(torch.autograd.Function):
             q, k, g, gs, dA, dq, dk,
             k.stride(1), k.stride(2), k.stride(3),
             T=T, K=K, BT=BT, BC=BC, BK=BK, NC=NC,
+            num_warps = num_warps,
+            num_stages = num_stages
         )
 
         du = g
@@ -880,6 +865,8 @@ class ChunkRWKV6Function(torch.autograd.Function):
             q.stride(1), q.stride(2), q.stride(3),
             v.stride(1), v.stride(2), v.stride(3), H=H,
             T=T, BT=BT, K=K, V=V, BK=next_pk_2, BV=next_pv_2,
+            num_warps = num_warps,
+            num_stages = num_stages
         )
         du = du.sum(2)
         return dq.to(dtype), dk.to(dtype), dv.to(dtype), dg.to(dtype), du.to(dtype), None, \
@@ -928,8 +915,6 @@ def chunk_rwkv6(
         scale = r.shape[-1] ** -0.5
     if u.dim() == 2:
         u = u.unsqueeze(0).repeat(r.shape[0], 1, 1)
-    if initial_state is None:
-        initial_state = torch.zeros(r.shape[0], r.shape[1], r.shape[-1], v.shape[-1], dtype=r.dtype, device=r.device)
     o, final_state = ChunkRWKV6Function.apply(r, k, v, g, u, scale, initial_state,
                                               output_final_state, checkpoint_level, training)
     return o, final_state

fla/ops/rwkv6/recurrent_naive.py
@@ -113,8 +113,10 @@ class NativeRecurrentRWKV6Function(torch.autograd.Function):
     @autocast_custom_fwd
     def forward(ctx, q, k, v, w, u, scale, initial_state, output_final_state: bool = False, training: bool = True):
         o, ht = naive_recurrent_rwkv6(q, k, v, w, u, scale, initial_state, output_final_state)
+        if initial_state is not None:
+            initial_state = initial_state.clone()
         if training:
-            ctx.save_for_backward(q, k, v, w, u, o, initial_state.clone())
+            ctx.save_for_backward(q, k, v, w, u, o, initial_state)
         return o, ht
 
     @staticmethod
@@ -162,8 +164,6 @@ def native_recurrent_rwkv6(
         scale = r.shape[-1] ** -0.5
     if u.dim() == 2:
         u = torch.broadcast_to(u.unsqueeze(0), (r.shape[0], *u.shape))
-    if initial_state is None:
-        initial_state = torch.zeros(r.shape[0], r.shape[1], r.shape[-1], v.shape[-1], dtype=r.dtype, device=r.device)
     o, final_state = NativeRecurrentRWKV6Function.apply(r, k, v, w, u, scale, initial_state, output_final_state, training)
 
     return o, final_state