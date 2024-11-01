fd2f88cbf0a90b35f5bac8db3f2c6c498a3ab7d7
yzhangcs <yzhang.cs@outlook.com>
2024-11-01 18:54:47 +0000

    [Retention] Update test cases

--- tests/ops/test_retention.py
+++ tests/ops/test_retention.py
@@ -8,12 +8,28 @@ from fla.ops.retention import (chunk_retention, fused_recurrent_retention,
 from fla.ops.retention.naive import naive_retention
 
 
+def get_abs_err(x, y):
+    return (x-y).flatten().abs().max().item()
+
+
+def get_err_ratio(x, y):
+    err = (x-y).flatten().square().mean().sqrt().item()
+    base = (x).flatten().square().mean().sqrt().item()
+    return err / base
+
+
+def assert_close(prefix, ref, tri, ratio):
+    msg = f"{prefix} diff: {get_abs_err(ref, tri):.6f} ratio: {get_err_ratio(ref, tri):.6f}"
+    print(msg)
+    assert get_err_ratio(ref, tri) < ratio, msg
+
+
 @pytest.mark.parametrize("B", [4])
 @pytest.mark.parametrize("H", [4])
 @pytest.mark.parametrize("T", [300, 512])
 @pytest.mark.parametrize("K", [32, 64, 100])
 @pytest.mark.parametrize("expand_ratio", [1, 2])
-@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.float32])
 def test_chunk(
     B: int,
     H: int,
@@ -23,7 +39,6 @@ def test_chunk(
     dtype: torch.dtype
 ):
     torch.manual_seed(42)
-    atol = 1e-3 if dtype == torch.float32 else 1e-1
     V = K * expand_ratio
 
     q = torch.randn((B, H, T, K), dtype=dtype, device='cuda').requires_grad_()
@@ -44,11 +59,12 @@ def test_chunk(
     tri_dq, q.grad = q.grad.clone(), None
     tri_dk, k.grad = k.grad.clone(), None
     tri_dv, v.grad = v.grad.clone(), None
-    assert ref.allclose(tri, 0, atol), f"o diff: {torch.abs(ref - tri).max()}"
-    assert ref_ht.allclose(tri_ht, 0, atol), f"o diff: {torch.abs(ref_ht - tri_ht).max()}"
-    assert ref_dq.allclose(tri_dq, 0, atol), f"dq diff: {torch.abs(ref_dq - tri_dq).max()}"
-    assert ref_dk.allclose(tri_dk, 0, atol), f"dk diff: {torch.abs(ref_dk - tri_dk).max()}"
-    assert ref_dv.allclose(tri_dv, 0, atol), f"dv diff: {torch.abs(ref_dv - tri_dv).max()}"
+
+    assert_close(" o", ref, tri, 0.005)
+    assert_close("ht", ref_ht, tri_ht, 0.005)
+    assert_close("dq", ref_dq, tri_dq, 0.005)
+    assert_close("dk", ref_dk, tri_dk, 0.005)
+    assert_close("dv", ref_dv, tri_dv, 0.005)
 
 
 @pytest.mark.parametrize("B", [4])
@@ -66,25 +82,26 @@ def test_parallel(
     dtype: torch.dtype
 ):
     torch.manual_seed(42)
-    atol = 1e-3 if dtype == torch.float32 else 1e-1
     V = K * expand_ratio
 
     q = torch.randn((B, H, T, K), dtype=dtype, device='cuda').requires_grad_()
     k = torch.randn((B, H, T, K), dtype=dtype, device='cuda').requires_grad_()
     v = torch.randn((B, H, T, V), dtype=dtype, device='cuda').requires_grad_()
     do = torch.randn_like(v)
+
     ref = naive_retention(q, k, v)
     ref.backward(do)
     ref_dq, q.grad = q.grad.clone(), None
     ref_dk, k.grad = k.grad.clone(), None
     ref_dv, v.grad = v.grad.clone(), None
 
-    tri = parallel_retention(q, k, v)
+    tri, _ = parallel_retention(q, k, v)
     tri.backward(do)
     tri_dq, q.grad = q.grad.clone(), None
     tri_dk, k.grad = k.grad.clone(), None
     tri_dv, v.grad = v.grad.clone(), None
-    assert ref.allclose(tri, 0, atol), f"o diff: {torch.abs(ref - tri).max()}"
-    assert ref_dq.allclose(tri_dq, 0, atol), f"dq diff: {torch.abs(ref_dq - tri_dq).max()}"
-    assert ref_dk.allclose(tri_dk, 0, atol), f"dk diff: {torch.abs(ref_dk - tri_dk).max()}"
-    assert ref_dv.allclose(tri_dv, 0, atol), f"dv diff: {torch.abs(ref_dv - tri_dv).max()}"
+
+    assert_close(" o", ref, tri, 0.005)
+    assert_close("dq", ref_dq, tri_dq, 0.005)
+    assert_close("dk", ref_dk, tri_dk, 0.005)
+    assert_close("dv", ref_dv, tri_dv, 0.005)
