f9c6e3ab8981e97245f44c2d4d37e3e38a197a04
sonta <bestsonta@gmail.com>
2024-11-02 18:12:43 -0400

    [DeltaNet] add naive parallel impl

--- fla/ops/delta_rule/naive.py
+++ fla/ops/delta_rule/naive.py
@@ -4,8 +4,10 @@ import torch
 from einops import rearrange
 
 
-def delta_rule_recurrence(q, k, v, beta):
+def delta_rule_recurrence(q, k, v, beta, initial_state=None, output_final_state=True):
+    orig_dtype = q.dtype
     b, h, l, d_k = q.shape
+    q, k, v, beta = map(lambda x: x.float(), [q, k, v, beta])
     d_v = v.shape[-1]
     o = torch.zeros_like(v)
     S = torch.zeros(b, h, d_k, d_v).to(v)
@@ -13,6 +15,9 @@ def delta_rule_recurrence(q, k, v, beta):
 
     if beta.ndim < v.ndim:
         beta = beta[..., None]
+
+    if initial_state is not None:
+        S += initial_state
 
     for i in range(l):
         _k = k[:, :, i]
@@ -23,8 +28,8 @@ def delta_rule_recurrence(q, k, v, beta):
         _v = _v * beta_i
         S = S.clone() + _k.unsqueeze(-1) * _v.unsqueeze(-2)
         o[:, :, i] = torch.einsum('bhd,bhdm->bhm', _q, S)
-
-    return o
+    S = None if output_final_state is False else S
+    return o.to(orig_dtype), S
 
 
 def delta_rule_chunkwise(q, k, v, beta, chunk_size=32):
@@ -36,41 +41,88 @@ def delta_rule_chunkwise(q, k, v, beta, chunk_size=32):
 
     assert l % chunk_size == 0
 
-    # note that diagonal is masked.
+    # compute (I - tri(diag(beta) KK^T))^{-1} 
     mask = torch.triu(torch.ones(chunk_size, chunk_size, dtype=torch.bool, device=q.device), diagonal=0)
     q, k, v, k_beta = map(lambda x: rearrange(x, 'b h (n c) d -> b h n c d', c=chunk_size), [q, k, v, k_beta])
     attn = -(k_beta @ k.transpose(-1, -2)).masked_fill(mask, 0)
-
     for i in range(1, chunk_size):
         attn[..., i, :i] = attn[..., i, :i] + (attn[..., i, :, None].clone() * attn[..., :, :i].clone()).sum(-2)
-
     attn = attn + torch.eye(chunk_size, dtype=torch.float, device=q.device)
-    # u
-    k_cumsum = attn @ v
-    # w
-    k_cumdecay = attn @ k_beta
-
-    v = k_cumsum
+    
+    u = attn @ v
+    w = attn @ k_beta
     S = k.new_zeros(b, h, d_k, d_v)
     o = torch.zeros_like(v)
     mask = torch.triu(torch.ones(chunk_size, chunk_size, dtype=torch.bool, device=q.device), diagonal=1)
     for i in range(0, l // chunk_size):
-        q_i, k_i, v_i = q[:, :, i], k[:, :, i], v[:, :, i]
+        q_i, k_i = q[:, :, i], k[:, :, i]
         attn = (q_i @ k_i.transpose(-1, -2)).masked_fill_(mask, 0)
-        v_prime = k_cumdecay[:, :, i] @ S
-        v_new = v_i - v_prime
+        u_i = u[:, :, i] - w[:, :, i] @ S
         o_inter = q_i @ S
-        o[:, :, i] = o_inter + attn @ v_new
-        # chunk state update
-        S = S + k_i.transpose(-1, -2) @ v_new
+        o[:, :, i] = o_inter + attn @ u_i
+        S = S + k_i.transpose(-1, -2) @ u_i
 
     return rearrange(o, 'b h n c d -> b h (n c) d')
+
+
+def delta_rule_parallel(q, k, v, beta, BM=128, BN=32):
+    b, h, l, d_k = q.shape
+    d_v = v.shape[-1]
+    q = q * (d_k ** -0.5)
+    v = v * beta[..., None]
+    k_beta = k * beta[..., None]
+
+    # compute (I - tri(diag(beta) KK^T))^{-1} 
+    q, k, v, k_beta = map(lambda x: rearrange(x, 'b h (n c) d -> b h n c d', c=BN), [q, k, v, k_beta])
+    mask = torch.triu(torch.ones(BN, BN, dtype=torch.bool, device=q.device), diagonal=0)
+    T = -(k_beta @ k.transpose(-1, -2)).masked_fill(mask, 0)
+    for i in range(1, BN):
+        T[..., i, :i] = T[..., i, :i].clone() + (T[..., i, :, None].clone() * T[..., :, :i].clone()).sum(-2)
+    T = T + torch.eye(BN, dtype=torch.float, device=q.device)
+
+    mask2 = torch.triu(torch.ones(BN, BN, dtype=torch.bool, device=q.device), diagonal=1)
+    A_local = (q @ k.transpose(-1,-2)).masked_fill(mask2, 0) @ T
+    o_intra = A_local @ v
+    
+    # apply cumprod transition matrices on k to the last position within the chunk
+    k = k - ((k @ k.transpose(-1, -2)).masked_fill(mask, 0) @ T).transpose(-1, -2) @ k_beta
+    # apply cumprod transition matrices on q to the first position within the chunk
+    q = q - A_local @ k_beta
+    o_intra = A_local @ v
+
+    A = torch.zeros(b, h, l, l, device=q.device)
+
+    q, k, v, k_beta, o_intra = map(lambda x: rearrange(x, 'b h n c d -> b h (n c) d'), [q, k, v, k_beta, o_intra])
+    o = torch.empty_like(v)
+    for i in range(0, L, BM):
+        q_i = q[:, :, i:i+BM]
+        o_i = o_intra[:, :, i:i+BM]
+        # intra block
+        for j in range(i + BM - 2 * BN, i-BN, -BN):
+            k_j = k[:, :, j:j+BN]
+            A_ij = q_i @ k_j.transpose(-1, -2)
+            mask = torch.arange(i, i+BM) >= (j + BN)
+            A_ij = A_ij.masked_fill_(~mask[:, None].to(A_ij.device), 0)
+            A[:, :, i:i+BM, j:j+BN] = A_ij
+            q_i = q_i - A_ij @ k_beta[:, :, j:j+BN]
+            o_i += A_ij @ v[:, :, j:j+BN]
+        # inter block
+        for j in range(i - BN, -BN, -BN):
+          k_j = k[:, :, j:j+BN]
+          A_ij = q_i @ k_j.transpose(-1, -2)
+          A[:, :, i:i+BM, j:j+BN] = A_ij
+          q_i = q_i - A_ij @ k_beta[:, :, j:j+BN]
+          o_i += A_ij @ v[:, :, j:j+BN]
+        o[:, :, i:i+BM] = o_i
+    
+    # (Optional) also return the attention matrix A
+    return o
 
 
 if __name__ == '__main__':
     B = 2
     H = 4
-    L = 256
+    L = 512
     DK = 128
     DV = 128
     q = (torch.randn(B, H, L, DK)).cuda().requires_grad_(True)
@@ -79,7 +131,7 @@ if __name__ == '__main__':
     v = (torch.randn(B, H, L, DV)).cuda().requires_grad_(True)
     beta = torch.randn(B, H, L).cuda().sigmoid().requires_grad_(True)
 
-    o = delta_rule_recurrence(q, k, v, beta)
+    o, _ = delta_rule_recurrence(q, k, v, beta)
     do = torch.randn(B, H, L, DV).cuda()
     o.backward(do, retain_graph=True)
     q_grad, q.grad = q.grad, None
@@ -94,4 +146,18 @@ if __name__ == '__main__':
     assert torch.allclose(k.grad, k_grad, atol=1e-4), breakpoint()
     assert torch.allclose(v.grad, v_grad, atol=1e-4), breakpoint()
     assert torch.allclose(beta.grad, beta_grad, atol=1e-4), breakpoint()
+
+    q_grad, q.grad = q.grad, None
+    k_grad, k.grad = k.grad, None
+    v_grad, v.grad = v.grad, None
+    beta_grad, beta.grad = beta.grad, None
+
+    o3 = delta_rule_parallel(q, k, v, beta)
+    o3.backward(do)
+    assert torch.allclose(o, o3, atol=1e-4), breakpoint()
+    assert torch.allclose(q.grad, q_grad, atol=1e-4), breakpoint()
+    assert torch.allclose(k.grad, k_grad, atol=1e-4), breakpoint()
+    assert torch.allclose(v.grad, v_grad, atol=1e-4), breakpoint()
+    assert torch.allclose(beta.grad, beta_grad, atol=1e-4), breakpoint()
+    
     print("All passed!")
