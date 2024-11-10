47f8ce6349884b2fdd7621490aa9dc89fd67edf1
Yu Zhang <yzhang.cs@outlook.com>
2024-11-10 14:12:21 +0800

    [Simple GLA] Update imports

--- fla/ops/simple_gla/__init__.py
+++ fla/ops/simple_gla/__init__.py
@@ -2,10 +2,10 @@
 
 from .chunk import chunk_simple_gla
 from .fused_recurrent import fused_recurrent_simple_gla
-from .parallel import ParallelSimpleGLAFunction
+from .parallel import parallel_simple_gla
 
 __all__ = [
     'chunk_simple_gla',
     'fused_recurrent_simple_gla',
-    'ParallelSimpleGLAFunction'
+    'parallel_simple_gla'
 ]
