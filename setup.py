c645ad1be0d2ff243578c51350fb8b6f38c5d7d9
yzhangcs <yzhang.cs@outlook.com>
2024-12-25 15:07:15 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -128,8 +128,9 @@ setup(
     ],
     python_requires='>=3.7',
     install_requires=[
+        'torch>=2.3',
+        'transformers>=4.40.0',
         'triton>=3.0',
-        'transformers',
         'datasets',
         'einops',
         'ninja'
