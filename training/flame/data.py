8a3093115788f1f42003abd28ce52053d1008dd0
yzhangcs <yzhang.cs@outlook.com>
2024-12-26 18:42:02 +0000

    [🔥] Enable returning preprocessed offsets

--- training/flame/data.py
+++ training/flame/data.py
@@ -11,8 +11,7 @@ from transformers import PreTrainedTokenizer
 @dataclass
 class DataCollatorForLanguageModeling:
     """
-    Data collator used for language modeling. Inputs are dynamically padded to the maximum length of a batch if they
-    are not all of the same length.
+    Data collator used for language modeling.
 
     Args:
         tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
@@ -25,14 +24,7 @@ class DataCollatorForLanguageModeling:
             If `False`, the `input_ids` with shape `[batch_size, seq_len]` will be returned directly.
         return_tensors (`str`):
             The type of Tensor to return. Allowable values are "pt".
-
-    <Tip>
-
-    For best performance, this data collator should be used with a dataset having items that are dictionaries or
-    BatchEncoding, with the `"special_tokens_mask"` key, as returned by a [`PreTrainedTokenizer`] or a
-    [`PreTrainedTokenizerFast`] with the argument `return_special_tokens_mask=True`.
-
-    </Tip>"""
+    """
 
     tokenizer: PreTrainedTokenizer
     varlen: bool = False
@@ -43,17 +35,28 @@ class DataCollatorForLanguageModeling:
         examples: List[Union[List[int], Dict[str, Any]]]
     ) -> Dict[str, Any]:
         if not isinstance(examples[0], Dict):
-            examples = [{'input_ids': x} for x in examples]
-        if isinstance(examples[0]['input_ids'], List):
-            examples = [{'input_ids': torch.tensor(x['input_ids'], dtype=torch.long)} for x in examples]
-        elif isinstance(examples[0]['input_ids'], np.ndarray):
-            examples = [{'input_ids': torch.from_numpy(x['input_ids'])} for x in examples]
+            examples = [{'input_ids': example} for example in examples]
+
+        def tensorize(example: Dict[str, Any]) -> Dict[str, Any]:
+            tensorized = {}
+            for key in ['input_ids', 'offsets']:
+                if key not in example:
+                    continue
+                if isinstance(example[key], List):
+                    tensorized[key] = torch.tensor(example[key], dtype=torch.long)
+                elif isinstance(example[key], np.ndarray):
+                    tensorized[key] = torch.from_numpy(example[key])
+            return tensorized
+
+        examples = list(map(tensorize, examples))
 
         if not self.varlen:
             length_of_first = examples[0]['input_ids'].size(0)
             # Check if padding is necessary.
-            if all(x['input_ids'].size(0) == length_of_first for x in examples):
-                batch = {'input_ids': torch.stack([x['input_ids'] for x in examples], dim=0)}
+            if all(example['input_ids'].size(0) == length_of_first for example in examples):
+                batch = {
+                    'input_ids': torch.stack([example['input_ids'] for example in examples], dim=0),
+                }
             else:
                 # If yes, check if we have a `pad_token`.
                 if self.tokenizer._pad_token is None:
@@ -63,22 +66,31 @@ class DataCollatorForLanguageModeling:
                     )
                 batch = self.tokenizer.pad(examples, return_tensors=self.return_tensors, return_attention_mask=False)
         else:
-            batch = {'input_ids': torch.cat([x['input_ids'] for x in examples], dim=0).unsqueeze(0)}
-            if self.tokenizer.add_bos_token:
-                offsets = []
-                if batch['input_ids'][0, 0] != self.tokenizer.bos_token_id:
-                    offsets.append(torch.tensor([0], dtype=torch.long))
-                offsets.append(torch.where(batch['input_ids'].eq(self.tokenizer.bos_token_id))[1])
-                offsets.append(torch.tensor([len(batch['input_ids'][0])], dtype=torch.long))
-                batch['offsets'] = torch.cat(offsets, dim=0)
-            elif self.tokenizer.add_eos_token:
-                offsets = [torch.tensor([0], dtype=torch.long)]
-                offsets.append(torch.where(batch['input_ids'].eq(self.tokenizer.eos_token_id))[1] + 1)
-                if batch['input_ids'][0, -1] != self.tokenizer.eos_token_id:
-                    offsets.append(torch.tensor([len(batch['input_ids'][0])], dtype=torch.long))
-                batch['offsets'] = torch.cat(offsets, dim=0)
+            if len(examples) > 1:
+                raise ValueError("The batch size must be 1 for variable length inputs.")
+            batch = {
+                'input_ids': torch.cat([example['input_ids'] for example in examples], dim=0).unsqueeze(0)
+            }
+            if 'offsets' in examples[0]:
+                batch['offsets'] = torch.cat([example['offsets'] for example in examples], dim=0).unsqueeze(0)
             else:
-                raise ValueError("You must allow the tokenizer to add either a bos or eos token as separators.")
+                # determine boundaries by bos/eos positions
+                if self.tokenizer.add_bos_token:
+                    offsets = []
+                    if batch['input_ids'][0, 0] != self.tokenizer.bos_token_id:
+                        offsets.append(torch.tensor([0], dtype=torch.long))
+                    offsets.append(torch.where(batch['input_ids'].eq(self.tokenizer.bos_token_id))[1])
+                    offsets.append(torch.tensor([len(batch['input_ids'][0])], dtype=torch.long))
+                    batch['offsets'] = torch.cat(offsets, dim=0)
+                elif self.tokenizer.add_eos_token:
+                    offsets = [torch.tensor([0], dtype=torch.long)]
+                    offsets.append(torch.where(batch['input_ids'].eq(self.tokenizer.eos_token_id))[1] + 1)
+                    if batch['input_ids'][0, -1] != self.tokenizer.eos_token_id:
+                        offsets.append(torch.tensor([len(batch['input_ids'][0])], dtype=torch.long))
+                    batch['offsets'] = torch.cat(offsets, dim=0)
+                else:
+                    raise ValueError("You must allow the tokenizer to add either a bos or eos token as separators.")
+
         labels = batch['input_ids'].clone()
         if self.tokenizer.pad_token_id is not None:
             labels[labels == self.tokenizer.pad_token_id] = -100
