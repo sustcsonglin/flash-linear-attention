[Gated DeltaNet] Fix nan and `dg` computation

fla/ops/gated_delta_rule/chunk.py
@@ -11,6 +11,7 @@ from fla.ops.gated_delta_rule.wy_fast import (bwd_prepare_wy_repr,
                                               fwd_prepare_wy_repr,
                                               fwd_recompute_w_u)
 from fla.ops.utils import chunk_local_cumsum
+from fla.ops.utils.safe import safe_diff_exp
 from fla.utils import autocast_custom_bwd, autocast_custom_fwd, contiguous
 
 
@@ -160,9 +161,6 @@ def chunk_gated_delta_rule_fwd_kernel_o(
         i_tg = i_b * NT + i_t
         bos, eos = i_b * T, i_b * T + T
 
-    o_i = tl.arange(0, BT)
-    m_s = o_i[:, None] >= o_i[None, :]
-
     b_o = tl.zeros([BT, BV], dtype=tl.float32)
     b_s = tl.zeros([BT, BT], dtype=tl.float32)
     for i_k in range(tl.cdiv(K, BK)):
@@ -192,9 +190,7 @@ def chunk_gated_delta_rule_fwd_kernel_o(
         p_o = tl.make_block_ptr(o + (bos * H + i_h) * V, (T, V), (H*V, 1), (i_t * BT, i_v * BV), (BT, BV), (1, 0))
     b_g = tl.load(p_g, boundary_check=(0,))
     b_o = b_o * tl.exp(b_g)[:, None]
-    b_s = b_s * tl.exp(b_g[:, None] - b_g[None, :])
-    b_s = tl.where(m_s, b_s, 0)
-
+    b_s = b_s * safe_diff_exp(b_g, BT, q_first=True)
     b_v = tl.load(p_v, boundary_check=(0, 1))
     b_o = (b_o + tl.dot(b_s.to(b_v.dtype), b_v, allow_tf32=False)) * scale
     tl.store(p_o, b_o.to(p_o.dtype.element_ty), boundary_check=(0, 1))
@@ -206,7 +202,7 @@ def chunk_gated_delta_rule_fwd_kernel_o(
 @triton.autotune(
     configs=[
         triton.Config({}, num_warps=num_warps)
-        for num_warps in [2, 4]
+        for num_warps in [1, 4]
     ],
     key=['BT', 'BK', 'BV'],
 )
@@ -258,8 +254,8 @@ def chunk_gated_delta_rule_fwd_kernel_prepare_dv(
         p_g = tl.make_block_ptr(g + bos * H + i_h, (T,), (H,), (i_t * BT,), (BT,), (0,))
 
     b_g = tl.load(p_g, boundary_check=(0,))
-    b_A = b_A * tl.exp(b_g[None, :] - b_g[:, None]) * scale
-    b_A = tl.where(tl.arange(0, BT)[:, None] <= tl.arange(0, BT)[None, :], b_A, 0).to(do.dtype.element_ty)
+    b_A = b_A * safe_diff_exp(b_g, BT, q_first=False) * scale
+    b_A = b_A.to(do.dtype.element_ty)
 
     for i_v in range(tl.cdiv(V, BV)):
         if HEAD_FIRST:
@@ -413,6 +409,7 @@ def chunk_gated_delta_rule_bwd_kernel_dqkw(
     offsets,
     indices,
     scale,
+    B: tl.constexpr,
     T: tl.constexpr,
     H: tl.constexpr,
     K: tl.constexpr,
@@ -426,6 +423,7 @@ def chunk_gated_delta_rule_bwd_kernel_dqkw(
 ):
     i_k, i_t, i_bh = tl.program_id(0), tl.program_id(1), tl.program_id(2)
     i_b, i_h = i_bh // H, i_bh % H
+    dg += i_k * B * T * H
     if USE_OFFSETS:
         i_tg = i_t
         i_n, i_t = tl.load(indices + i_t * 2).to(tl.int32), tl.load(indices + i_t * 2 + 1).to(tl.int32)
@@ -517,16 +515,18 @@ def chunk_gated_delta_rule_bwd_kernel_dqkw(
     b_dw *= b_g_exp_qw[:, None]
     b_dg -= tl.sum(b_dw * b_w, axis=1)
     b_dk *= tl.exp(b_g_last - b_g)[:, None]
+
     b_dg -= tl.sum(b_dk * b_k, axis=1)
     b_dg_last += tl.sum(b_dk * b_k)
     b_g_exp_qw = None
     # [BT, BT]
-    b_ds = tl.where(o_i[:, None] >= o_i[None, :], b_ds * scale * tl.exp(b_g[:, None] - b_g[None, :]), 0).to(b_q.dtype)
+    b_ds = b_ds * scale * safe_diff_exp(b_g, BT, q_first=True)
     # gradient wrt
     b_dg_mask = tl.dot(b_q, tl.trans(b_k), allow_tf32=False) * b_ds
     b_dg += tl.sum(b_dg_mask, axis=1)
     b_dg -= tl.sum(b_dg_mask, axis=0)
     # [BT, BK]
+    b_ds = b_ds.to(b_k.dtype)
     b_dq += tl.dot(b_ds, b_k, allow_tf32=False)
     b_dk += tl.trans(tl.dot(tl.trans(b_q), b_ds, allow_tf32=False))
     b_dg = tl.where(o_i < min(BT, T-i_t*BT) - 1, b_dg, b_dg + b_dg_last)
@@ -768,7 +768,6 @@ def chunk_gated_delta_rule_fwd_o(
     BK = min(triton.next_power_of_2(K), 64)
     BV = min(triton.next_power_of_2(V), 64)
     NV = triton.cdiv(V, BV)
-
     o = torch.empty_like(v_new)
     grid = (NV, NT, B * H)
     chunk_gated_delta_rule_fwd_kernel_o[grid](
@@ -828,7 +827,7 @@ def chunk_gated_delta_rule_bwd_dqkwg(
     dq = torch.empty_like(q)
     dk = torch.empty_like(k)
     dw = torch.empty_like(w)
-    dg = torch.empty_like(g)
+    dg = torch.empty(NK, *g.shape, dtype=torch.float32, device=g.device)
     grid = (NK, NT, B * H)
     chunk_gated_delta_rule_bwd_kernel_dqkw[grid](
         q=q,
@@ -847,6 +846,7 @@ def chunk_gated_delta_rule_bwd_dqkwg(
         offsets=offsets,
         indices=indices,
         scale=scale,
+        B=B,
         T=T,
         H=H,
         K=K,
@@ -857,6 +857,7 @@ def chunk_gated_delta_rule_bwd_dqkwg(
         NT=NT,
         HEAD_FIRST=head_first
     )
+    dg = dg.sum(0)
     return dq, dk, dw, dg
 
 
@@ -1062,11 +1063,9 @@ class ChunkDeltaRuleFunction(torch.autograd.Function):
             head_first=head_first,
             chunk_size=chunk_size
         )
-        ctx.save_for_backward(q, k, v, g, beta, Aw, Au, initial_state)
+        ctx.save_for_backward(q, k, v, g, beta, Aw, Au, initial_state, offsets, indices)
         ctx.chunk_size = chunk_size
         ctx.scale = scale
-        ctx.offsets = offsets
-        ctx.indices = indices
         ctx.head_first = head_first
         return o.to(q.dtype), final_state
 
@@ -1078,7 +1077,7 @@ class ChunkDeltaRuleFunction(torch.autograd.Function):
         do: torch.Tensor,
         dht: torch.Tensor
     ):
-        q, k, v, g, beta, Aw, Au, initial_state = ctx.saved_tensors
+        q, k, v, g, beta, Aw, Au, initial_state, offsets, indices = ctx.saved_tensors
         dq, dk, dv, db, dg, dh0 = chunk_gated_delta_rule_bwd(
             q=q,
             k=k,
@@ -1091,8 +1090,8 @@ class ChunkDeltaRuleFunction(torch.autograd.Function):
             initial_state=initial_state,
             do=do,
             dht=dht,
-            offsets=ctx.offsets,
-            indices=ctx.indices,
+            offsets=offsets,
+            indices=indices,
             head_first=ctx.head_first,
             chunk_size=ctx.chunk_size
         )

tests/ops/test_gated_delta.py
@@ -2,12 +2,13 @@
 
 import os
 
-from einops import rearrange
 import pytest
 import torch
 import torch.nn.functional as F
+from einops import rearrange
 
-from fla.ops.gated_delta_rule import chunk_gated_delta_rule, fused_recurrent_gated_delta_rule
+from fla.ops.gated_delta_rule import (chunk_gated_delta_rule,
+                                      fused_recurrent_gated_delta_rule)
 
 
 def get_abs_err(x, y):
@@ -70,14 +71,13 @@ def recurrent_gated_delta_rule_ref(
     return o, S
 
 
-
 def chunk_gated_delta_rule_ref(
     q: torch.Tensor,
     k: torch.Tensor,
     v: torch.Tensor,
     g: torch.Tensor,
     beta: torch.Tensor,
-    chunk_size: int = 64,  
+    chunk_size: int = 64,
     scale: float = None,
     initial_state: torch.Tensor = None,
     output_final_state: bool = False,
@@ -96,7 +96,7 @@ def chunk_gated_delta_rule_ref(
 
     T = q.shape[-2]
     pad_len = (BT - (T % BT)) % BT
-    if pad_len > 0:            
+    if pad_len > 0:
         # Pad all tensors
         q = F.pad(q, (0, 0, 0, pad_len))
         k = F.pad(k, (0, 0, 0, pad_len))
@@ -114,10 +114,12 @@ def chunk_gated_delta_rule_ref(
     assert l % chunk_size == 0
     # note that diagonal is masked.
     mask = torch.triu(torch.ones(chunk_size, chunk_size, dtype=torch.bool, device=q.device), diagonal=0)
-    q, k, v, k_beta, decay = map(lambda x: rearrange(x, 'b h (n c) d -> b h n c d',
-                                c=chunk_size), [q, k, v, k_beta, decay.unsqueeze(-1)])
+    q, k, v, k_beta, decay = map(
+        lambda x: rearrange(x, 'b h (n c) d -> b h n c d', c=chunk_size),
+        [q, k, v, k_beta, decay.unsqueeze(-1)]
+    )
     decay = decay.squeeze(-1).cumsum(-1)
-    L_mask = (decay.unsqueeze(-1) - decay.unsqueeze(-2)).exp()
+    L_mask = ((decay.unsqueeze(-1) - decay.unsqueeze(-2)).tril().exp().float()).tril()
     attn = -((k_beta @ k.transpose(-1, -2)) * L_mask).masked_fill(mask, 0)
     for i in range(1, chunk_size):
         attn[..., i, :i] = attn[..., i, :i].clone() + (attn[..., i, :i, None].clone() * attn[..., :i, :i].clone()).sum(-2)
@@ -163,6 +165,7 @@ def chunk_gated_delta_rule_ref(
 @pytest.mark.parametrize("scale", [1])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
 @pytest.mark.parametrize("head_first", [True, False])
+@pytest.mark.parametrize("gate_logit_normalizer", [0.05, 1, 20])
 def test_recurrent_forward(
     B: int,
     T: int,
@@ -170,20 +173,22 @@ def test_recurrent_forward(
     D: int,
     scale: float,
     dtype: torch.dtype,
-    head_first: bool
+    head_first: bool,
+    gate_logit_normalizer: float
 ):
     if head_first:
         q = torch.randn(B, H, T, D, dtype=dtype)
         k = F.normalize(torch.randn(B, H, T, D, dtype=torch.float32), p=2, dim=-1).to(dtype)
         v = torch.randn(B, H, T, D, dtype=dtype)
         beta = torch.rand(B, H, T, dtype=dtype).sigmoid()
-        g = F.logsigmoid(torch.rand(B, H, T, dtype=dtype))
+        g = F.logsigmoid(torch.rand(B, H, T, dtype=torch.float32))
     else:
         q = torch.randn(B, T, H, D, dtype=dtype)
         k = F.normalize(torch.randn(B, T, H, D, dtype=torch.float32), p=2, dim=-1).to(dtype)
         v = torch.randn(B, T, H, D, dtype=dtype)
         beta = torch.rand(B, T, H, dtype=dtype).sigmoid()
-        g = F.logsigmoid(torch.rand(B, T, H, dtype=dtype))
+        g = F.logsigmoid(torch.rand(B, T, H, dtype=torch.float32))
+    g = g / gate_logit_normalizer
     h0 = torch.randn(B, H, D, D, dtype=torch.float32)
     q, k, v, beta, g, h0 = map(lambda x: x.cuda().requires_grad_(), (q, k, v, beta, g, h0))
     ref, ref_ht = recurrent_gated_delta_rule_ref(
@@ -213,10 +218,11 @@ def test_recurrent_forward(
 
 
 @pytest.mark.parametrize("B", [2])
-@pytest.mark.parametrize("T", [1, 7, 15, 63, 286, 300])
-@pytest.mark.parametrize("H", [2, 16])
+@pytest.mark.parametrize("gate_logit_normalizer", [0.05, 1, 20])
 @pytest.mark.parametrize("D", [50, 100, 200])
-@pytest.mark.parametrize("scale", [1])
+@pytest.mark.parametrize("H", [2])
+@pytest.mark.parametrize("T", [7, 63, 286, 300, 1000])
+@pytest.mark.parametrize("scale", [1, 0.1])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
 @pytest.mark.parametrize("head_first", [True, False])
 def test_chunk(
@@ -226,22 +232,24 @@ def test_chunk(
     D: int,
     dtype: torch.dtype,
     scale: float,
-    head_first: bool
+    head_first: bool,
+    gate_logit_normalizer: float
 ):
     if head_first:
         q = torch.randn(B, H, T, D, dtype=dtype)
         k = F.normalize(torch.randn(B, H, T, D, dtype=torch.float32), p=2, dim=-1).to(dtype)
         v = torch.randn(B, H, T, D, dtype=dtype)
         beta = torch.rand(B, H, T, dtype=dtype).sigmoid()
-        g = F.logsigmoid(torch.rand(B, H, T, dtype=dtype))
-        h0 = torch.randn(B, H, D, D, dtype=torch.float32)
+        g = F.logsigmoid(torch.rand(B, H, T, dtype=torch.float32))
+        h0 = torch.zeros(B, H, D, D, dtype=torch.float32)
     else:
         q = torch.randn(B, T, H, D, dtype=dtype)
         k = F.normalize(torch.randn(B, T, H, D, dtype=torch.float32), p=2, dim=-1).to(dtype)
         v = torch.randn(B, T, H, D, dtype=dtype)
         beta = torch.rand(B, T, H, dtype=dtype).sigmoid()
-        g = F.logsigmoid(torch.rand(B, T, H, dtype=dtype))
-        h0 = torch.randn(B, H, D, D, dtype=torch.float32)
+        g = F.logsigmoid(torch.rand(B, T, H, dtype=torch.float32))
+        h0 = torch.zeros(B, H, D, D, dtype=torch.float32)
+    g = g / gate_logit_normalizer
     q, k, v, beta, g, h0 = map(lambda x: x.cuda().requires_grad_(True), (q, k, v, beta, g, h0))
 
     tri, tri_ht = chunk_gated_delta_rule(
@@ -258,8 +266,8 @@ def test_chunk(
     do = torch.randn_like(v)
     dht = torch.randn_like(h0)
     ((tri * do).sum() + (tri_ht * dht).sum()).backward(retain_graph=True)
-    tri_dq, tri_dk, tri_dv, tri_dbeta, tri_dh0 = q.grad, k.grad, v.grad, beta.grad, h0.grad
-    q.grad = k.grad = v.grad = beta.grad = h0.grad = None
+    tri_dq, tri_dk, tri_dv, tri_dbeta, tri_dg, tri_dh0 = q.grad, k.grad, v.grad, beta.grad, g.grad, h0.grad
+    q.grad = k.grad = v.grad = beta.grad = g.grad = h0.grad = None
 
     ref, ref_ht = chunk_gated_delta_rule_ref(
         q.clone(),
@@ -274,22 +282,23 @@ def test_chunk(
     )
 
     ((ref * do).sum() + (ref_ht * dht).sum()).backward(retain_graph=True)
-    ref_dq, ref_dk, ref_dv, ref_dbeta, ref_dh0 = q.grad, k.grad, v.grad, beta.grad, h0.grad
+    ref_dq, ref_dk, ref_dv, ref_dbeta, ref_dg, ref_dh0 = q.grad, k.grad, v.grad, beta.grad, g.grad, h0.grad
     assert_close("  o", ref, tri, 0.005)
     assert_close(" ht", ref_ht, tri_ht, 0.005)
-    assert_close(" dq", ref_dq, tri_dq, 0.007)
+    assert_close(" dq", ref_dq, tri_dq, 0.008)
     assert_close(" dk", ref_dk, tri_dk, 0.008)
-    assert_close(" dv", ref_dv, tri_dv, 0.007)
-    assert_close(" db", ref_dbeta, tri_dbeta, 0.007)
-    assert_close("dh0", ref_dh0, tri_dh0, 0.007)
-
+    assert_close(" dv", ref_dv, tri_dv, 0.008)
+    assert_close(" db", ref_dbeta, tri_dbeta, 0.02)
+    assert_close("dh0", ref_dh0, tri_dh0, 0.008)
+    if gate_logit_normalizer >= 1 and ref_dg.norm() > 0.01:
+        assert_close("dg", ref_dg, tri_dg, 0.02)
 
 
 @pytest.mark.parametrize("N", [4])
 @pytest.mark.parametrize("T", [64, 128, 200, 250, 256, 300, 400, 512, 1000, 2048])
 @pytest.mark.parametrize("H", [2, 16])
 @pytest.mark.parametrize("D", [50, 100, 200])
-@pytest.mark.parametrize("scale", [1])
+@pytest.mark.parametrize("scale", [1, 0.1])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
 def test_chunk_varlen(
     N: int,
@@ -331,8 +340,8 @@ def test_chunk_varlen(
         head_first=False
     )
     ((tri * do).sum() + (tri_ht * dht).sum()).backward(retain_graph=True)
-    tri_dq, tri_dk, tri_dv, tri_dbeta, tri_dh0 = q.grad, k.grad, v.grad, beta.grad, h0.grad
-    q.grad = k.grad = v.grad = beta.grad = h0.grad = None
+    tri_dq, tri_dk, tri_dv, tri_dbeta, tri_dg, tri_dh0 = q.grad, k.grad, v.grad, beta.grad, g.grad, h0.grad
+    q.grad = k.grad = v.grad = beta.grad = g.grad = h0.grad = None
 
     ref = []
     ref_ht = []
@@ -350,16 +359,17 @@ def test_chunk_varlen(
         )
         ref.append(ref_i)
         ref_ht.append(ref_ht_i)
-    ref =  torch.cat(ref, 1)
+    ref = torch.cat(ref, 1)
     ref_ht = torch.cat(ref_ht, 0)
 
     ((ref * do).sum() + (ref_ht * dht).sum()).backward(retain_graph=True)
-    ref_dq, ref_dk, ref_dv, ref_dbeta, ref_dh0 = q.grad, k.grad, v.grad, beta.grad, h0.grad
+    ref_dq, ref_dk, ref_dv, ref_dbeta, ref_dg, ref_dh0 = q.grad, k.grad, v.grad, beta.grad, g.grad, h0.grad
 
     assert_close("  o", ref, tri, 0.005)
     assert_close(" ht", ref_ht, tri_ht, 0.005)
     assert_close(" dq", ref_dq, tri_dq, 0.007)
     assert_close(" dk", ref_dk, tri_dk, 0.008)
     assert_close(" dv", ref_dv, tri_dv, 0.007)
-    assert_close(" db", ref_dbeta, tri_dbeta, 0.007)
+    assert_close(" db", ref_dbeta, tri_dbeta, 0.015)
+    assert_close(" dg", ref_dg, tri_dg, 0.015)
     assert_close("dh0", ref_dh0, tri_dh0, 0.007)